28d6cb8b7b791d52736a794a1c97400ee378e6e5
Mouad Abouhali <mouad.abouhali@airbus.com>
2015-06-16 16:35:35 +0200

    IDA BinCAT plugin

---
+++ python/idabincat/bincat.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python 
+# Filename : mymodule.py 
+
+#Imports
+from idaapi import * 
+from idc import * 
+from idautils import *
+
+# Functions 
+def test():
+    print("This is an IDA test Module")
+
+
+
+def getCurrentaddress() : 
+    print("Current address is : %s")%(hex(here()).rstrip('L'))
+     
+
+def getAllfunctions() :
+    print("Listing all functions ")
+    # idautils necessary 
+    ea = ScreenEA() 
+    for funcea in Functions(SegStart(ea),SegEnd(ea)):
+        print("Function %s at 0x%x")%(GetFunctionName(funcea),funcea)
+
+
+'''
+AddComment : pour ajouter un commentaire  
+params : address format hex (0xFF...)
+          string : commentaire 
+'''
+
+def AddComment( address  , string  ) :
+     # check that input address is in format : 0xXXXX
+     MakeComm(address, string)                                                                    
+
