000ae31fc5bb11106c2f5d41fc55947903184fa1
Mouad Abouhali <mouad.abouhali@airbus.com>
2015-08-27 17:37:52 +0200

    Ajout de la fonction GetFileInfo

--- python/idabincat/bincat.py
+++ python/idabincat/bincat.py
@@ -9,7 +9,7 @@ import threading
 import time
 import socket 
 import os 
-
+from collections import OrderedDict
 
 exitFlag =0 
 
@@ -52,6 +52,56 @@ def AddComment( address  , string  ) :
      # check that input address is in format : 0xXXXX
      MakeComm(address, string)                                                                    
 
+'''
+Params : No 
+Return : an OrderedDict that contains the following informations 
+         filename : 
+         imagebase 
+         entrypoints (name , address )
+         segments  (name , start address, end address, class )
+'''
+def GetFileInfo() :
+    ordinals =  [] 
+    entrypoints = []
+    main_entry_point_adr = 0x00
+    main_entry_point_name = "" 
+    
+    for i in range(0,GetEntryPoinQty()):
+        ordinals.append(GetEntryOrdinal(i))
+
+    for i in ordinals:
+        entrypoints.append( (i,GetEntryPoint(i)) ) 
+
+    for i in entrypoints:
+        if GetFunctionName(i[0]) == "start":
+            main_entry_point_adr =  hex(i[1]).rstrip('L')
+            main_entry_point_name= "start" 
+ 
+
+    data_to_send["filename"] = idaapi.get_root_filename()
+    data_to_send["imagebase"] = idaapi.get_imagebase() 
+    data_to_send["entrypoints"] = (main_entry_point_name,main_entry_point_adr)
+
+
+    i = 0 
+    for seg in Segments():
+        seg_attributes = idc.GetSegmentAttr(SegStart(seg),idc.SEGATTR_TYPE)
+        if seg_attributes  == idaapi.SEG_NORM : data_to_send["segment_"+str(i)] = ( SegName(seg), hex(SegStart(seg)).rstrip('L'), hex(SegEnd(seg)).rstrip('L')  ,"UNKNOWN_TYPE")
+        if seg_attributes  == idaapi.SEG_XTRN : data_to_send["segment_"+str(i)] = ( SegName(seg), hex(SegStart(seg)).rstrip('L'), hex(SegEnd(seg)).rstrip('L')  ,"EXTERN")
+        if seg_attributes  == idaapi.SEG_CODE : data_to_send["segment_"+str(i)] = ( SegName(seg), hex(SegStart(seg)).rstrip('L'), hex(SegEnd(seg)).rstrip('L')  ,"CODE")
+        if seg_attributes  == idaapi.SEG_DATA : data_to_send["segment_"+str(i)] = ( SegName(seg), hex(SegStart(seg)).rstrip('L'), hex(SegEnd(seg)).rstrip('L')  ,"DATA")
+        if seg_attributes  == idaapi.SEG_IMP :  data_to_send["segment_"+str(i)] = ( SegName(seg), hex(SegStart(seg)).rstrip('L'), hex(SegEnd(seg)).rstrip('L')  ,"JAVA_IMPLEMENTATION")
+        if seg_attributes  == idaapi.SEG_GRP :  data_to_send["segment_"+str(i)] = ( SegName(seg), hex(SegStart(seg)).rstrip('L'), hex(SegEnd(seg)).rstrip('L')  ,"GROUP_SEGMENTS")
+        if seg_attributes  == idaapi.SEG_NULL : data_to_send["segment_"+str(i)] = ( SegName(seg), hex(SegStart(seg)).rstrip('L'), hex(SegEnd(seg)).rstrip('L')  ,"ZERO_LEN")
+        if seg_attributes  == idaapi.SEG_UNDF : data_to_send["segment_"+str(i)] = ( SegName(seg), hex(SegStart(seg)).rstrip('L'), hex(SegEnd(seg)).rstrip('L')  ,"UNDEFINED_SEGMENT")
+        if seg_attributes  == idaapi.SEG_BSS :  data_to_send["segment_"+str(i)] = ( SegName(seg), hex(SegStart(seg)).rstrip('L'), hex(SegEnd(seg)).rstrip('L')  ,"BSS")
+        if seg_attributes  == idaapi.SEG_ABSSYM : data_to_send["segment_"+str(i)] = ( SegName(seg), hex(SegStart(seg)).rstrip('L'), hex(SegEnd(seg)).rstrip('L')  ,"SEGMENT_WITH_ABSOLUTE_SYMBOLS")
+        if seg_attributes  == idaapi.SEG_COMM :   data_to_send["segment_"+str(i)] = ( SegName(seg), hex(SegStart(seg)).rstrip('L'), hex(SegEnd(seg)).rstrip('L')  ,"SEGMENT_WITH_COMMUNAL_DEFS")
+        if seg_attributes  == idaapi.SEG_IMEM :  data_to_send["segment_"+str(i)] = ( SegName(seg), hex(SegStart(seg)).rstrip('L'), hex(SegEnd(seg)).rstrip('L')  ,"IDA_INTERN")
+        i+=1
+
+    return(data_to_send)
+
 
 # Classes 
 
@@ -74,6 +124,8 @@ class BinCATThread (threading.Thread):
                 message = " toto "
                 print("[+] IDA plugin --> analyzer : %s")%message
                 child.send(message) 
+                fileinfo = GetFileInfo()
+                print(fileinfo)
                 message = child.recv(1024)
                 print("[+] message received from analyzer %s ")%message   
 
