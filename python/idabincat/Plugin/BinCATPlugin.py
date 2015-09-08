01362b6c4273a9eb148130dabe46b1d7edef12f2
Mouad Abouhali <mouad.abouhali@airbus.com>
2015-09-08 18:23:59 +0200

    BinCAT.py graphical plugin POC working

--- python/idabincat/Plugin/BinCATPlugin.py
+++ python/idabincat/Plugin/BinCATPlugin.py
@@ -9,53 +9,80 @@ Edit --> plugins
 
 import idaapi 
 
-class HTooltip(idaapi.action_handler_t):
+
+#Configuration Handler
+class HTooltipC(idaapi.action_handler_t):
     def __init__(self):
         idaapi.action_handler_t.__init__(self)
 
     def activate(self, ctx):
-        idaapi.warning("Tooltip-providing action triggered")
+        idaapi.warning("Configuration : Tooltip-providing action triggered")
         return 1
 
     def update(self, ctx):
         return idaapi.AST_ENABLE_ALWAYS
 
 
+#Analyse Handler 
+class HTooltipA(idaapi.action_handler_t):
+    def __init__(self):
+        idaapi.action_handler_t.__init__(self)
+
+    def activate(self, ctx):
+        idaapi.warning("Analyse : Tooltip-providing action triggered")
+        idaapi.msg(" Analyzer handler %s "%type(ctx))
+        idaapi.msg(" Analyzer handler current ea %s "%hex(ctx.cur_ea))
+        for fdix in ctx.chooser_selection: 
+            idaapi.msg(" fdix %s "%type(fdix))
+        return 1
+
+    def update(self, ctx):
+        return idaapi.AST_ENABLE_ALWAYS
+
+
+
 class Hooks(idaapi.UI_Hooks):
     def populating_tform_popup(self, form, popup):
+        idaapi.msg("Hooks called\n")
         idaapi.attach_action_to_popup(form, popup, "my:tooltip0", "BinCAT/Config/", idaapi.SETMENU_APP)
-        idaapi.attach_action_to_popup(form, popup, "my:tooltip1", "BinCAT/Analyse/Taint register", idaapi.SETMENU_APP)
+        idaapi.attach_action_to_popup(form, popup, "my:tooltip1", "BinCAT/Analyse/", idaapi.SETMENU_APP)
 
 
 class bincat_plugin_t(idaapi.plugin_t):
-    flags = idaapi.PLUGIN_UNL
+    flags = idaapi.PLUGIN_FIX
     comment = "BinCAT is cool ! "
 
     help = "This is help"
     wanted_name = "BinCAT IDA Plugin"
     wanted_hotkey = "Alt-F8"
+    hooks = None 
 
     def init(self):
         idaapi.msg("init() called!\n")
-        tooltip_act0 = idaapi.action_desc_t('my:tooltip0', 'BinCAT configuration ', HTooltip(), '', 'This is a tooltip-providing action tooltip', -1)
-        tooltip_act1 = idaapi.action_desc_t('my:tooltip1', 'BinCAT analyser ', HTooltip(), '', 'This is yet another a tooltip-providing action tooltip', -1)
+        self.hooks = Hooks()
+        return idaapi.PLUGIN_KEEP
+
+    def run(self, arg):
+        idaapi.msg("run() called with %d!\n" % arg)
+        tooltip_act0 = idaapi.action_desc_t('my:tooltip0', 'BinCAT : Configuration ', HTooltipC(), '', 'This is a tooltip-providing action tooltip', -1)
+        tooltip_act1 = idaapi.action_desc_t('my:tooltip1', 'BinCAT : Taint register ', HTooltipA(), '', 'This is yet another a tooltip-providing action tooltip', -1)
         idaapi.register_action(tooltip_act0)
         idaapi.register_action(tooltip_act1)
         idaapi.attach_action_to_menu("View/", 'my:tooltip0', idaapi.SETMENU_APP)
         idaapi.attach_action_to_menu("View/", 'my:tooltip1', idaapi.SETMENU_APP)
+
         idaapi.open_disasm_window("BinCAT Plugin-view")
 
-        hooks = Hooks()
-        hooks.hook()
-
-
-        return idaapi.PLUGIN_OK
-
-    def run(self, arg):
-        idaapi.msg("run() called with %d!\n" % arg)
+        #hooks = Hooks()
+        idaapi.msg("Calling Hooks\n")
+        self.hooks.hook()
 
     def term(self):
         idaapi.msg("term() called!\n")
+        if self.hooks is not None :
+            self.hooks.unhook()
+            self.hooks =  None 
+    
         #TO DO unhook actions 
 
 def PLUGIN_ENTRY():
