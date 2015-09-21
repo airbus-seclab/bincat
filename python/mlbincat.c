64c52ee64bc60d7e03bfc3bc64ce30608beaf1f7
Xavier Mehrenberger <xavier.mehrenberger@airbus.com>
2015-09-21 17:20:03 +0200

    mlbincat: raise Python exceptions when an uncaught OCaml exception is received.

--- python/mlbincat.c
+++ python/mlbincat.c
@@ -8,7 +8,10 @@
 #include <caml/mlvalues.h>
 #include <caml/memory.h>
 #include <caml/callback.h>
+#include <caml/printexc.h>
 
+
+static PyObject *OcamlException;
 
 static int load_caml(value **func, char *funcname)
 {
@@ -26,7 +29,7 @@ static int load_caml(value **func, char *funcname)
 static PyObject *process(PyObject *self, PyObject *args)
 {
   static value *process_func;
-  
+
   char *configfile;
   char *resultfile;
 
@@ -34,24 +37,37 @@ static PyObject *process(PyObject *self, PyObject *args)
     return NULL;
   if (!load_caml(&process_func, "process"))
     return NULL;
- 
-  caml_callback2(*process_func, caml_copy_string(configfile), caml_copy_string(resultfile));
+
+  value res;
+  res = caml_callback2_exn(*process_func, caml_copy_string(configfile), caml_copy_string(resultfile));
+  if (Is_exception_result(res)) {
+    value exn = Extract_exception(res);
+	PyErr_SetString(OcamlException, (*caml_format_exception)(exn));
+	return NULL;
+  }
   return Py_None;
 }
 
 
 static PyMethodDef MlMethods[] = {
-  { "process", process, METH_VARARGS, "Lacunh the analyzer" },
+  { "process", process, METH_VARARGS, "Launch the analyzer" },
   {NULL, NULL, 0, NULL}
 };
- 
+
 PyMODINIT_FUNC
 initmlbincat(void)
 {
-	char *argv[2];	
+	char *argv[2];
 	argv[0]="";
 	argv[1]=NULL;
 	caml_startup(argv);
-	(void) Py_InitModule("mlbincat", MlMethods);
+	PyObject *module;
+	module = Py_InitModule("mlbincat", MlMethods);
+	if (module == NULL)
+	  return;
+
+	OcamlException = PyErr_NewException("mlbincat.OcamlException", NULL, NULL);
+	Py_INCREF(OcamlException);
+	PyModule_AddObject(module, "error", OcamlException);
 }
 
