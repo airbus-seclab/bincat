c0ad129c136e13c0880f1d2d56014465ba426be8
Xavier Mehrenberger <xavier.mehrenberger@airbus.com>
2016-01-18 14:25:10 +0100

    Add unit tests - first instruction.

---
+++ ocaml/test/test_instructions.py
@@ -0,0 +1,166 @@
+#!/usr/bin/env python2
+# This file describes tests for single instructions
+
+import pytest
+import ConfigParser
+import logging
+import sys
+
+
+class AnalyzerConfig(object):
+    """
+    TODO move to separate file, re-use in IDA plugin
+    """
+    def __init__(self):
+        #: self.eip[EIP value] contains a State object
+        self.stateAtEip = {}
+
+    def setBinaryFromString(self, string):
+        # TODO
+        pass
+
+    def setStatesFromAnalyzerOutput(self, filename):
+        # Parse output ini file
+        config = ConfigParser.ConfigParser()
+        config.read(filename)
+        for section in config.sections():
+            if not section.startswith('address = '):
+                logging.error("Unrecognized section in output file: %s",
+                              section)
+                sys.exit(1)
+            zxidx = section.index("0x") # [address = 0xFF..FF]
+            address = int(section[zxidx:], 16)
+            state = State(address)
+            state.setFromAnalyzerOutput(config.items(section))
+            self.stateAtEip[address] = state
+
+    def exportToFile(self, filename, eip):
+        # TODO
+        pass
+
+
+class State(object):
+    """
+    TODO move to separate file, re-use in IDA plugin
+    TODO separate computed state from user-set state
+    """
+    def __init__(self, address):
+        self.address = ""
+        #: self.ptrs[memory address or pointer name] =
+        #: ("memory zone", int value)
+        self.ptrs = {}
+        #: self.tainting[memory address or pointer name] =
+        #: taint value (object)?
+        self.tainting = {}
+
+    def __eq__(self, other):
+        result = False
+        if set(self.ptrs.keys()) != set(other.ptrs.keys()):
+            # might have to be refined
+            logging.error("different set of keys between states")
+            return False
+        for ptr in self.ptrs.keys():
+            if (self.ptrs[ptr] != other.ptrs[ptr]):
+                return False
+        if set(self.tainting.keys()) != set(other.tainting.keys()):
+            # might have to be refined
+            logging.error("different set of keys between states")
+            return False
+        for t in (set(self.tainting.keys()) | set(other.tainting.keys())):
+            if self.tainting[t] != other.tainting[t]:
+                return False
+        return True
+
+    def setFromAnalyzerOutput(self, outputkv):
+        """
+        :param outputkv: list of (key, value) tuples for each property set by
+            the analyzer at this EIP
+        """
+        for k, v in outputkv:
+            if k.startswith('tainting '):
+                self.tainting[k[9:]] = Tainting.fromAnalyzerOutput(v)
+            elif k.startswith('pointer '):
+                self.ptrs[k[8:]] = PtrValue.fromAnalyzerOutput(v)
+            else:
+                logging.error("Unrecognized key while parsing state: %s", k)
+                sys.exit(1)
+
+
+class PtrValue(object):
+    def __init__(self, zone, value):
+        self.zone = zone
+        self.value = value
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __eq__(self, other):
+        return self.zone == other.zone and self.value == other.value
+
+    @classmethod
+    def fromAnalyzerOutput(cls, s):
+        z, v = s[1:-1].split(',')
+        v = int(v, 16)
+        return cls(z, v)
+
+
+class Tainting(object):
+    def __init__(self, tainting):
+        self.tainting = tainting
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __eq__(self, other):
+        return self.tainting == other.tainting
+
+    @classmethod
+    def fromAnalyzerOutput(cls, s):
+        return cls(s)
+
+
+@pytest.fixture(scope='function', params=['template0.ini'])
+def initialState(request):
+    # TODO generate instead of using a fixed file, using States class
+    # (not implemented yet)
+    return open(request.param, 'rb').read()
+
+
+@pytest.fixture(scope='function')
+def analyzer(tmpdir):
+    import mlbincat
+
+    def run_analyzer(initialState, binarystr):
+        """
+        Create .ini and .bin
+        Run analyzer, get resulting state.
+        """
+        oldpath = tmpdir.chdir()
+        initfile = str(tmpdir.join('init.ini'))
+        with open(initfile, 'w+') as f:
+            f.write(initialState)
+        binfile = str(tmpdir.join('file.bin'))
+        with open(binfile, 'w+') as f:
+            f.write(binarystr)
+
+        # TODO write to init
+        outputfile = str(tmpdir.join('end.ini'))
+        mlbincat.process(initfile, outputfile)
+        ac = AnalyzerConfig()
+        ac.setStatesFromAnalyzerOutput(outputfile)
+        oldpath.chdir()
+        return ac
+    return run_analyzer
+
+
+def test_nop(analyzer, initialState):
+    ac = analyzer(initialState, binarystr='\x90')
+    assert ac.stateAtEip[0x00] == ac.stateAtEip[0x01]
+
+
+def test_pushebp(analyzer, initialState):
+    ac = analyzer(initialState, binarystr='\x55')
+    assert ac.stateAtEip[0x01].ptrs['reg[eax]'] == \
+        ac.stateAtEip[0x00].ptrs['reg[eax]'] + 1
+    # TODO check stack
+    # TODO check that nothing else has changed
