f9bc91be5c6ee4716c38119be151fba6ded6f367
Sarah Zennou <sarah.zennou@airbus.com>
2016-04-14 19:48:52 +0200

    improve output of bit vector

---
+++ ocaml/src/domains/value.ml
@@ -0,0 +1,137 @@
+(** data type *)
+type t =
+  | BOT (** bottom (undef) *)
+  | ZERO   (** zero *)
+  | ONE   (** one *)
+  | TOP (** top *)
+      
+let bot = BOT
+let is_bot v = v = BOT
+ 		     
+let join b1 b2 =
+  match b1, b2 with
+  | ZERO, ZERO 	    	  -> ZERO
+  | ONE, ONE 	    	  -> ONE
+  | BOT, v | v, BOT 	  -> v
+  | ONE, ZERO | ZERO, ONE -> ONE
+  | _, _ 	    	  -> TOP
+			 
+let meet b1 b2 =
+  match b1, b2 with
+  | ZERO, ZERO 	    	  -> ZERO
+  | ONE, ONE 	    	  -> ONE
+  | b, TOP | TOP, b 	  -> b
+  | ONE, ZERO | ZERO, ONE -> ZERO
+  | _, _ 	    	  -> BOT
+			 
+let to_string b =
+  match b with
+  | TOP  -> "?"
+  | BOT  -> "_"
+  | ZERO -> "0"
+  | ONE  -> "1"
+	     
+let equal b1 b2 = b1 = b2
+			 
+let add b1 b2 =
+  match b1, b2 with
+  | TOP, _ | _, TOP 	  -> TOP, true
+  | BOT, _ | _, BOT 	  -> BOT, true
+  | ZERO, ZERO 	    	  -> ZERO, false
+  | ZERO, ONE | ONE, ZERO -> ONE, false
+  | ONE, ONE 	    	  -> ZERO, true
+
+let sub b1 b2 =
+  match b1, b2 with
+  | TOP, _ | _, TOP -> TOP, true
+  | BOT, _ | _, BOT -> BOT, true
+  | ZERO, ZERO 	    -> ZERO, false
+  | ZERO, ONE       -> ONE, true
+  | ONE, ZERO       -> ONE, false
+  | ONE, ONE 	    -> ZERO, false
+								
+let xor b1 b2 =
+  match b1, b2 with
+  | TOP, _ | _, TOP 	  -> TOP
+  | BOT, _ | _, BOT 	  -> BOT
+  | ZERO, ZERO 	    	  -> ZERO
+  | ZERO, ONE | ONE, ZERO -> ONE
+  | ONE, ONE 	    	  -> ZERO
+			 
+let neg v =
+  match v with
+  | BOT  -> BOT
+  | TOP  -> TOP
+  | ZERO -> ONE
+  | ONE  -> ZERO
+		     
+(* finite lattice => widen = join *)
+let widen = join
+	      
+(* conversion to Z.t. May raise an exception if the conversion fails *)
+let to_value v =
+  match v with
+  | BOT | TOP -> raise Exceptions.Concretization
+  | ZERO      -> Z.zero
+  | ONE       -> Z.one
+   
+let default = BOT
+		
+let eq v1 v2 =
+  match v1, v2 with
+  | BOT, BOT 	    	      -> true
+  | _, BOT | BOT, _ 	      -> false
+  | TOP, _ | _, TOP 	      -> true
+  | ZERO, ZERO | ONE, ONE     -> true
+  | _, _ 	    	      -> false
+			 
+let neq v1 v2 =
+  match v1, v2 with
+  | BOT, BOT 	    	      -> true
+  | _, BOT | BOT, _ 	      -> false
+  | TOP, _ | _, TOP 	      -> true
+  | ZERO, ZERO | ONE, ONE     -> false
+  | _, _ 	    	      -> true
+			 
+let leq v1 v2 =
+  match v1, v2 with
+  | BOT, _ 	       		      -> true
+  | _, BOT             		      -> false
+  | TOP, _ | _, TOP    		      -> true
+  | ZERO, ZERO | ONE, ONE | ZERO, ONE -> true
+  | _, _ 	       		      -> false
+			    
+let lt v1 v2 =
+  match v1, v2 with
+  | _, BOT          -> false
+  | BOT, _          -> true
+  | TOP, _ | _, TOP -> true
+  | ZERO, ONE       -> true
+  | _, _ 	    -> false
+			 
+let geq v1 v2 =
+  match v1, v2 with
+  | BOT, BOT 	       		      -> true
+  | TOP, _ | _, TOP    		      -> true
+  | ZERO, ZERO | ONE, ONE | ONE, ZERO -> true
+  | _, _ 	       		      -> false
+			    
+let gt v1 v2 =
+  match v1, v2 with
+  | BOT, _          -> false
+  | _, BOT          -> true
+  | TOP, _ | _, TOP -> true
+  | ONE, ZERO       -> true
+  | _, _ 	    -> false
+
+		       
+let compare v1 op v2 =
+  match op with
+  | Asm.EQ  -> eq v1 v2
+  | Asm.NEQ -> neq v1 v2
+  | Asm.LEQ -> leq v1 v2
+  | Asm.GEQ -> geq v2 v1
+  | Asm.LT  -> lt v1 v2
+  | Asm.GT  -> gt v1 v2
+		  
+let subset v1 v2 = leq v1 v2
---
+++ ocaml/src/domains/vector.ml
@@ -0,0 +1,328 @@
+(** vector lifting of a value_domain *)
+(** binary operations are supposed to apply on operands of the same length *)
+
+(** signature of value domain *)
+module type Val =
+  sig
+    (** abstract data type *)
+    type t
+    (** bottom *) 
+    val bot: t
+    (** default value *)
+    val default: t
+    (** conversion to value of type Z.t. May raise an exception *)
+    val to_value: t -> Z.t
+    (** conversion from Z.t value *)
+    val of_value: Z.t -> t
+    (** taint conversion from Z.t value *)
+    val taint_of_value: Z.t -> t
+    (** abstract join *)
+    val join: t -> t -> t
+    (** abstract meet *)
+    val meet: t -> t -> t
+    (** string conversion *)
+    val to_string: t -> string
+    (** add operation. The optional return value is None when no carry *)
+    (** occurs in the result and Some c with c the carry value otherwise *)
+    val add: t -> t -> t * (t option)
+    (** sub operation. The optional return value is None when no borrow *)
+    (** occurs and Some b with b the borrow value otherwise *)
+    val sub: t -> t -> t * (t option)
+    (** xor operation *)
+    val xor: t -> t -> t
+    (** logical and *)
+    val logand: t -> t -> t
+    (** logical or *)
+    val logor: t -> t -> t
+    (** bit neg *)
+    val neg: t -> t
+    (** untaint *)
+    val untaint: t -> t
+    (** abstract value of 1 *)
+    val one: t
+    (** comparison to one *)
+    val is_one: t -> bool
+    (** abstract value of 0 *)
+    val zero: t
+    (** comparison to zero *)
+    val is_zero: t -> bool
+    (** strictly less than comparison *)
+    val lt: t -> t -> bool
+    (** check whether the first abstract value is included in the second one *)
+    val subset: t -> t -> bool
+    (** comparison *)
+    val compare: t -> Asm.cmp -> t -> bool
+  end
+    
+(** signature of vector *)
+module type T =
+  sig
+    (** abstract data type *)
+    type t
+    (** default value *)
+    val default: int -> t
+    (** value conversion. May raise an exception *)
+    val to_value: t -> Z.t
+    (** abstract join *)
+    val join: t -> t -> t
+    (** abstract meet *)
+    val meet: t -> t -> t
+    (** string conversion *)
+    val to_string: t -> string
+    (** binary operation *)
+    val binary: Asm.binop -> t -> t -> t
+    (** unary operation *)
+    val unary: Asm.unop -> t -> t
+    (** untaint *)
+    val untaint: t -> t
+    (** conversion from word *)
+    val of_word: Data.Word.t -> t
+    (** comparison *)
+    val compare: t -> Asm.cmp -> t -> bool
+    (** conversion to a set of addresses *)
+    val to_addresses: Data.Address.region -> t -> Data.Address.Set.t
+    (** check whether the first argument is included in the second one *)
+    val subset: t -> t -> bool
+    (** conversion from a config value *)
+    (** the integer parameter is the size in bits of the config value *)
+    val of_config: Config.cvalue -> int -> t
+    (** conversion from a tainting value *)
+    val taint_of_config: Config.tvalue -> int -> t
+    (** [combine v1 v2 l u] computes v1[l, u] <- v2 *)
+    val combine: t -> t -> int -> int -> t
+  end
+    
+module Make(V: Val) =
+  (struct
+    type t = V.t array
+
+    let map2 f v1 v2 =
+      let n = Array.length v1        in
+      let v = Array.make n V.default in
+      for i = 0 to n-1 do
+	v.(i) <- f v1.(i) v2.(i)
+      done;
+      v
+
+    let for_all2 p v1 v2 =
+      try
+	for i = 0 to (Array.length v1)-1 do
+	  if not (p v1.(i) v2.(i)) then raise Exit
+	done;
+	true
+      with Exit -> false
+
+    let for_all p v =
+      try
+	for i = 0 to (Array.length v) -1 do
+	  if not (p v.(i)) then raise Exit
+	done;
+	true
+      with Exit -> false
+		     
+    let join v1 v2 = map2 V.join v1 v2
+
+    let meet v1 v2 = map2 V.meet v1 v2
+
+    let to_string v =
+      let v = Array.fold_left (fun s v -> s ^ (V.to_string v)) "" v      in
+      let t = Array.fold_left (fun s v -> s ^(V.string_of_taint t)) "" v in
+      if String.length t = 0 then v
+      else Printf.sprintf "%s ! %s" v t
+
+    (* common utility to add and sub *)
+    let core_add_sub op v1 v2 =
+      let n = Array.length v1        in
+      let v = Array.make n V.default in
+      let carry_borrow = ref None in
+      for i = 0 to n - 1 do
+	let c = 
+	  (* add the carry/borrow if present *)
+	  match !carry_borrow with
+	  | None -> None
+	  | Some b' -> let b', c' = op v.(i) b' in v.(i) <- b'; c'
+	in
+	(* compute the ith bit of the result with the ith bit of the operand *)
+	let b, c' = op v1.(i) v2.(i) in
+	v.(i) <- b;
+	(* update the new carry/borrow *)
+	match c with
+	| Some _ -> carry_borrow := c
+	| None   -> carry_borrow := c' (* correct as we know that we cannot have both cpred = Some ... and c' = Some ... *)
+      done;
+      v
+	     
+    let add v1 v2 = core_add_sub V.add v1 v2
+    let sub v1 v2 = core_add_sub V.sub v1 v2
+
+    let xor v1 v2 = map2 V.xor v1 v2
+    let logand v1 v2 = map2 V.logand v1 v2
+    let logor v1 v2 = map2 V.logor v1 v2
+
+			   
+    let zero_extend v n =
+      let n' = Array.length v in
+      if n <= n' then
+	v
+      else
+	let o  = n - n'              in
+	let v' = Array.make n V.zero in
+	for i = 0 to n'-1 do
+	  v'.(i+o) <- v.(i)
+	done;
+	v'
+
+	  
+    let shl v i = 
+      let n  = Array.length v      in
+      let v' = Array.make n V.zero in
+      for j = n-1 downto n-i+1 do
+	v'.(j-i) <- v.(j)
+      done;
+      v'
+
+    let shr v i =
+      let n  = Array.length v      in
+      let v' = Array.make n V.zero in
+      for j = 0 to i-1 do
+	v'.(j+i) <- v.(j)
+      done;
+      v'
+
+    let mul v1 v2 =
+      let n   = 2*(Array.length v1) in
+      let v   = Array.make n V.zero in
+      let v2' = zero_extend v2 n    in
+      let rec loop i v =
+	if i = 0 then
+	  v
+	else
+	  let v' =
+	    if V.is_one v1.(i) then add v (shl v2' i)
+	    else v
+	  in
+	  loop (i-1) v'
+      in
+      loop (n-1) v
+
+    (** return v1 / v2, modulo *)
+    let core_div v1 v2 =
+      (* check first that v2 is not zero *)
+      if for_all V.is_zero v2 then
+	Log.error "Division by zero"
+      else
+	let n   = Array.length v1        in
+	let v   = Array.make n V.default in
+	let one = Array.make n V.one     in 
+	let rec loop v r =
+	  if for_all2 (fun b1 b2 -> V.lt b1 b2) r v2 then
+	    loop (add v one) (sub r v2)
+	  else
+	    v, r
+	in
+	loop v v1
+	
+	     
+    let div v1 v2 = fst (core_div v1 v2)
+				     
+    let modulo v1 v2 = snd (core_div v1 v2)
+	
+    let binary op v1 v2 =
+      match op with
+      | Asm.Add -> add v1 v2
+      | Asm.Sub -> sub v1 v2
+      | Asm.Xor -> xor v1 v2
+      | Asm.And -> logand v1 v2
+      | Asm.Or  -> logor v1 v2
+      | Asm.Mul -> mul v1 v2
+      | Asm.Div -> div v1 v2
+      | Asm.Mod -> modulo v1 v2
+			  
+
+	  
+    let sign_extend v i =
+      let n = Array.length v in
+      if n >= i then
+	v
+      else
+	let sign = v.(0) in
+	let o    = n - i in
+	let v' =
+	  if V.is_zero sign then Array.make (n+i) V.zero
+	  else Array.make (n+i) V.one
+	in
+	for j = 0 to n-1 do
+	  v'.(j+o) <- v.(j)
+	done;
+	v'
+	
+    let unary op v =
+      match op with
+      | Asm.Not       -> Array.map V.neg v
+      | Asm.Shl i     -> shl v i
+      | Asm.Shr i     -> shr v i
+      | Asm.SignExt i -> sign_extend v i
+	   
+    let default sz = Array.make sz V.default
+
+    let untaint v = Array.map V.untaint v
+
+    let nth_of_value v i = Z.logand (Z.shift_right v i) Z.one
+				    
+    let of_word w =
+      let sz = Data.Word.size w	       in
+      let w' = Data.Word.to_int w      in
+      let r  = Array.make sz V.default in
+      for i = 0 to sz-1 do
+	r.(i) <- if Z.compare (nth_of_value w' i) Z.one = 0 then V.one else V.zero
+      done;
+      r
+
+    let to_value v =
+      let z = ref Z.zero in
+      for i = 0 to (Array.length v) - 1 do
+	let n = V.to_value v.(i) in
+	z := Z.add n (Z.shift_left !z 1)
+      done;
+      !z
+   
+    let to_word v = Data.Word.of_int (to_value v) (Array.length v)
+      
+    let to_addresses r v = Data.Address.Set.singleton (r, to_word v)
+
+    let subset v1 v2 = for_all2 V.subset v1 v2
+
+    let of_config c n =
+      let v = Array.make n V.default in
+      for i = 0 to n-1 do
+	v.(i) <- V.of_value (nth_of_value c i)
+      done;
+      v
+	
+    let taint_of_config t n =
+      match t with
+      | Config.Bits b ->
+	 let v = Array.make n V.default in
+	 for i = 0 to n-1 do
+	   v.(i) <- V.taint_of_value (nth_of_value b i)
+	 done;
+	 v
+      | Config.MBits (b, m) ->
+	 let v = Array.make n V.default in
+	 for i = 0 to n-1 do
+	   let bnth = nth_of_value b i in
+	   let mnth = nth_of_value m i in
+	   v.(i) <- V.join (V.taint_of_value bnth) (V.taint_of_value mnth)
+	 done;
+	 v
+
+    let combine v1 v2 l u =
+      let v = Array.copy v1 in
+      for i = l to u do
+	v.(i) <- v2.(i)
+      done;
+      v
+
+    let compare v1 op v2 = for_all2 (fun b1 b2 -> V.compare b1 op b2) v1 v2
+      
+  end: T)
