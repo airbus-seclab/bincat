38f0016b3cff6cdcb3e9895d125aeaa87c98b85f
Xavier Mehrenberger <xavier.mehrenberger@airbus.com>
2016-05-06 10:35:40 +0200

    idabincat.analyzer: improve argument parsing.

--- python/idabincat/analyzer.py
+++ python/idabincat/analyzer.py
@@ -11,36 +11,35 @@ def main():
     logger = logging.getLogger('BinCAT Analyzer')
     analyzer_dir = os.path.dirname(sys.argv[0]) + '/analyzer_log.txt'
 
-    print(analyzer_dir)
-
     logfile = logging.FileHandler(analyzer_dir)
     formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s    ')
 
     logfile.setFormatter(formatter)
     logger.addHandler(logfile)
-    logger.setLevel(logging.INFO)
 
-    print("\n[*] Analyzer: Entering main function. \n")
     argsParser = argparse.ArgumentParser(
         description="Analyzer arguments parsing")
     argsParser.add_argument(
-        '--inifile', action='store', help='Analyzer configuration file')
+        '--inifile', '-i', type=str, help='Analyzer configuration file',
+        required=True)
     argsParser.add_argument(
-        '--outfile', action='store', help='Analyzer output file')
+        '--outfile', '-o', type=str, help='Analyzer output file',
+        required=True)
     argsParser.add_argument(
-        '--logfile', action='store', help='Analyzer log file ')
+        '--logfile', '-l', type=str, help='Analyzer log file', required=True)
+    argsParser.add_argument(
+        "--verbose", "-v", action="count", default=0,
+        help="Be more verbose (can be used several times).")
 
     args = argsParser.parse_args()
+
+    # default: WARNING (30), -v once to reach INFO (20)
+    logger.setLevel(max(1, 30-10*args.verbose))
 
     logger.info("[Analyzer] Parsing command line:")
     logger.info("[Analyzer] ini file: %s ", args.inifile)
     logger.info("[Analyzer] out file: %s ", args.outfile)
     logger.info("[Analyzer] log file: %s ", args.logfile)
-
-    print("[*] Analyzer: processing the following parameters:\n")
-    print(" - inifile: %s.\n" % args.inifile)
-    print(" - outfile: %s.\n" % args.outfile)
-    print(" - logfile: %s.\n" % args.logfile)
 
     from pybincat import state
 
