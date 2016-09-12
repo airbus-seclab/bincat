b8e0482a2c735e548d3e69ef24764f9162f59bab
Raphaël Rigo <raphael.rigo@airbus.com>
2016-09-12 13:21:14 +0200

    Use tainted from state in plugin

--- python/idabincat/bcplugin.py
+++ python/idabincat/bcplugin.py
@@ -335,24 +335,14 @@ class State(object):
                 # address already seen, is tainted
                 continue
             ea = addr.value
+            tainted = False
             for n_id in nodeids:
                 # is it tainted?
                 # find children state
                 state = cfa[n_id]
-                children = cfa.edges[n_id]
-                tainted = False
-                for cnode in children:
-                    cstate = cfa[cnode]
-                    for k in state.list_modified_keys(cstate):
-                        if k.is_tainted():
-                            tainted = True
-                            break
-                        val = cstate[k]
-                        if val.is_tainted():
-                            tainted = True
-                            break
-                    if tainted:
-                        break
+                if state.tainted:
+                    tainted = True
+                    break
 
             if tainted:
                 idaapi.set_item_color(ea, 0xDDFFDD)
