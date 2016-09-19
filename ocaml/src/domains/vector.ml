eaaa873e201cc2797a67c71569a6b89c5042cf5e
Raphaël Rigo <raphael.rigo@airbus.com>
2016-09-19 15:54:55 +0200

    Revert logic and add checks in Vector.combine

--- ocaml/src/domains/vector.ml
+++ ocaml/src/domains/vector.ml
@@ -462,11 +462,11 @@ module Make(V: Val) =
         (** copy bits from v2 to bits from low to up of v1,
          *  vectors can be of different sizes *)
         let combine v1 v2 low up =
-            Log.debug (Printf.sprintf "Vector.combine : v1 = %s" (to_string v1));
-            Log.debug (Printf.sprintf "Vector.combine : v2 = %s" (to_string v2));
+            Log.debug (Printf.sprintf "Vector.combine : v1 = %s (%d)" (to_string v1) (Array.length v1));
+            Log.debug (Printf.sprintf "Vector.combine : v2 = %s (%d)" (to_string v2) (Array.length v2));
             Log.debug (Printf.sprintf "Vector.combine : low = %d, up = %d" low up);
-            if low > up then
-                Log.error "Vector.combine : low > up"
+            if low > up || (up-low+1) > (Array.length v2) then
+                Log.error "Vector.combine : low > up or length of src < up-low+1 "
             else
                 let sz1 = Array.length v1 in
                 let sz2 = Array.length v2 in
@@ -475,7 +475,7 @@ module Make(V: Val) =
                 else
 		  begin
 		    let v = Array.copy v1 in
-		    let j = ref (sz2-1-up) in
+		    let j = ref 0 in
 		    for i = (sz1-1-up) to (sz1-1-low) do
 		      v.(i) <- v2.(!j);
 		      j := !j+1;
