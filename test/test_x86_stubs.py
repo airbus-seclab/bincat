c814e1b9bcde529e1d92c545fc135a352e38a92a
phil <phil@secdev.org>
2017-06-19 18:12:37 +0200

    Added sprintf unit tests on number formats (%d, %i, %x, %lx)

--- test/test_x86_stubs.py
+++ test/test_x86_stubs.py
@@ -54,3 +54,33 @@ def test_printf_string(tmpdir, val, numl, numr):
     assert expected == bc.get_stdout(), (repr(fmtstr)+"\n"+bc.listing)
     assert len(expected) == bc.result.last_reg("eax").value, (repr(fmtstr)+"\n"+bc.listing)
 
+
+@pytest.mark.parametrize("val",[0, 1, 0x1f2, 0x1fa4, 0x45f672, 0x8245fa3d, 0xffffffff])
+@pytest.mark.parametrize("fmt",[xfail("i"),
+                                "x",
+                                xfail("d"),
+                                xfail("lx")])
+@pytest.mark.parametrize("mod",["",
+                                xfail("+"),
+                                xfail("-")])
+@pytest.mark.parametrize("zeropad",["","0"])
+@pytest.mark.parametrize("sz",["","1","5","8","13"])
+def test_sprintf_num(tmpdir, val, fmt, mod, zeropad, sz):
+    fmtstr = "TEST[%{mod}{zeropad}{sz}{fmt}]".format(**locals())
+    asm = """
+        push {val:#x} 
+        push 0x200      ; {fmt!r}
+        push 0x100
+        call 0x80000001
+    """.format(val=val, fmt=fmtstr)
+
+    bc = x86.make_bc_test(tmpdir, asm)
+    bc.initfile.set_mem(0x200, fmtstr+"\0")
+    bc.initfile.set_mem(0x100, "\0"*100)
+    bc.run()
+
+    expected = fmtstr % val
+    
+    assert expected == bc.result.last_state.get_string("g",0x100), (repr(fmtstr)+"\n"+bc.listing)
+    assert len(expected) == bc.result.last_reg("eax").value, (repr(fmtstr)+"\n"+bc.listing)
+
