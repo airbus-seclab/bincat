2277d963cad6d8c00c641af02bddc4ba52e08095
phil <phil@secdev.org>
2017-06-19 18:03:43 +0200

    Shortcut pytest.mark.xfail into xfail in test_x86_stub

--- test/test_x86_stubs.py
+++ test/test_x86_stubs.py
@@ -3,14 +3,16 @@ from util import X86
 
 x86 = X86("x86_stub.ini.in")
 
+xfail = pytest.mark.xfail
+
 @pytest.mark.parametrize("val",[0, 1, 0x1f2, 0x1fa4, 0x45f672, 0x8245fa3d, 0xffffffff])
-@pytest.mark.parametrize("fmt",[pytest.mark.xfail("i"),
+@pytest.mark.parametrize("fmt",[xfail("i"),
                                 "x",
-                                pytest.mark.xfail("d"),
-                                pytest.mark.xfail("lx")])
+                                xfail("d"),
+                                xfail("lx")])
 @pytest.mark.parametrize("mod",["",
-                                pytest.mark.xfail("+"),
-                                pytest.mark.xfail("-")])
+                                xfail("+"),
+                                xfail("-")])
 @pytest.mark.parametrize("zeropad",["","0"])
 @pytest.mark.parametrize("sz",["","1","5","8","13"])
 def test_printf_num(tmpdir, val, fmt, mod, zeropad, sz):
