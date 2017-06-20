4b5323b00377f8a2ca9fcd687bfeb54d64790252
phil <phil@secdev.org>
2017-06-20 17:22:55 +0200

    Fix sprintf %s unit test

--- test/test_x86_stubs.py
+++ test/test_x86_stubs.py
@@ -92,20 +92,22 @@ def test_sprintf_num(tmpdir, val, fmt, mod, zeropad, sz):
 @pytest.mark.parametrize("numl",["", xfail("0"), "2", "5"])
 @pytest.mark.parametrize("numr",["" ,xfail(".0"), xfail(".2"), xfail(".5")])
 @pytest.mark.parametrize("val",["","abc", "abcdefghi"])
-def test_printf_string(tmpdir, val, numl, numr):
+def test_sprintf_string(tmpdir, val, numl, numr):
     fmtstr = "TEST[%{numl}{numr}s]".format(**locals())
     asm = """
         push 0x300      ; {val!r}
         push 0x200      ; {fmt!r}
-        call 0x80000000
+        push 0x100
+        call 0x80000001
     """.format(val=val, fmt=fmtstr)
 
     bc = x86.make_bc_test(tmpdir, asm)
     bc.initfile.set_mem(0x200, fmtstr+"\0")
     bc.initfile.set_mem(0x300, val+"\0")
+    bc.initfile.set_mem(0x100, "\0"*100)
     bc.run()
 
     expected = fmtstr % val
     
-    assert expected == bc.get_stdout(), (repr(fmtstr)+"\n"+bc.listing)
+    assert expected == bc.result.last_state.get_string("g",0x100), (repr(fmtstr)+"\n"+bc.listing)
     assert len(expected) == bc.result.last_reg("eax").value, (repr(fmtstr)+"\n"+bc.listing)
