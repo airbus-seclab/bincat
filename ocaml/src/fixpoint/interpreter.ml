579958b19df96d015693df08617a47cbcba855ed
phil <phil@secdev.org>
2017-06-27 18:39:34 +0200

    Have interpreter.process_value catch Empty exception and return domain bottom instead

--- ocaml/src/fixpoint/interpreter.ml
+++ ocaml/src/fixpoint/interpreter.ml
@@ -204,6 +204,7 @@ module Make(D: Domain.T): (T with type domain = D.t) =
     
     let rec process_value (d: D.t) (s: Asm.stmt) (fun_stack: fun_stack_t) =
         L.debug (fun p -> p "process_value ---------\n%s\n---------\n%s\n---------" (String.concat " " (D.to_string d)) (Asm.string_of_stmt s true));
+      try
         let res, tainted = 
             match s with
             | Nop 				 -> d, false
@@ -266,7 +267,7 @@ module Make(D: Domain.T): (T with type domain = D.t) =
                (* fun_stack := List.tl !fun_stack; *)
             | _ 				 -> raise Jmp_exn
           in L.debug (fun p -> p "process_value returns taint : %B"  tainted); res, tainted
-						     
+      with Exceptions.Empty _ -> D.bot,false
     and process_if (d: D.t) (e: Asm.bexp) (then_stmts: Asm.stmt list) (else_stmts: Asm.stmt list) fun_stack =
       if has_jmp then_stmts || has_jmp else_stmts then
              raise Jmp_exn
