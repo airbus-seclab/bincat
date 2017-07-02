3c7cd8e142ade14dde8b7b8aa18c7b4c6a68fd84
phil <phil@secdev.org>
2017-07-02 14:50:59 +0200

    Add some unit tests for amrv7 condition codes predicates

--- test/test_armv7_isn.py
+++ test/test_armv7_isn.py
@@ -22,3 +22,49 @@ def test_assign(tmpdir):
         mov r2, r1
     """
     compare(tmpdir, asm, ["r0","r1", "r2"])
+
+
+
+##  ___   _ _____ _     ___ ___  ___   ___ 
+## |   \ /_\_   _/_\   | _ \ _ \/ _ \ / __|
+## | |) / _ \| |/ _ \  |  _/   / (_) | (__ 
+## |___/_/ \_\_/_/ \_\ |_| |_|_\\___/ \___|
+## 
+## DATA PROC
+
+def test_mov_reg(tmpdir):
+    asm = """
+            mov r0, #0x12
+            movs r1, r0
+    """
+    compare(tmpdir, asm, ["r0","r1", "z"])
+
+def test_mov_set_zflag(tmpdir):
+    asm = """
+            mov r1, #0
+            mov r2, #0
+            mov r3, #0
+            mov r4, #0
+            movs r0, #0x12
+            moveq r1, #1
+            movne r2, #1
+            movs r0, #0
+            moveq r3, #1
+            movne r4, #1
+    """
+    compare(tmpdir, asm, ["r0","r1","r2","r3", "r4", "n", "z"])
+
+def test_mov_set_vflag(tmpdir):
+    asm = """
+            mov r1, #0
+            mov r2, #0
+            mov r3, #0
+            mov r4, #0
+            movs r0, #0x12
+            movmi r1, #1
+            movpl r2, #1
+            movs r0, #0x80000000
+            movmi r3, #1
+            movpl r4, #1
+    """
+    compare(tmpdir, asm, ["r0","r1","r2","r3", "r4", "n", "z"])
