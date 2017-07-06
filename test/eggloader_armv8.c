5633bd892fa97161fe9ea505b0281f95fa9c143d
Raphaël Rigo <raphael.rigo@airbus.com>
2017-07-06 10:48:42 +0200

    Workaround GCC bug in armv8 test

--- test/eggloader_armv8.c
+++ test/eggloader_armv8.c
@@ -20,9 +20,9 @@ int main(int argc, char *argv[])
 {
         int f;
         int len;
-        unsigned int nzcv;
         unsigned long r0, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15,
                 r16, r17, r18, r19, r20, r21, r22, r23, r24, r25, r26, r27, r28, r29, r30;
+        unsigned int nzcv;
 
         char ret_to_main[] =
                 "\xfe\x07\x41\xf8"     //   ldr     x30, [sp],#16
