4dcdb34e4d0fa75902a54880164167c4b0778d32
Raphaël Rigo <raphael.rigo@airbus.com>
2017-07-06 14:35:58 +0200

    Base tests for armv8

--- test/test_armv8_isn.py
+++ test/test_armv8_isn.py
@@ -7,6 +7,9 @@ arm = AARCH64(
 )
 compare = arm.compare
 
+dataop_comp_logic = pytest.mark.parametrize("op", ["and", "eor", "orr", "bic"])
+dataop_comp_arith = pytest.mark.parametrize("op", ["sub", "rsb", "add"])
+
 def test_nop(tmpdir):
     asm = """
         nop
@@ -20,4 +23,22 @@ def test_assign(tmpdir):
         mov w2, w1
     """
     compare(tmpdir, asm, ["r0","r1","r2"])
+
+@dataop_comp_logic
+def test_data_proc_logic(tmpdir, op, armv7op, armv7op_):
+    asm = """
+            mov x0, #{armv7op}
+            mov x1, #{armv7op_}
+            {op} x2, x0, x1
+    """.format(**locals())
+    compare(tmpdir, asm, ["x0","x1", "x2", "x3", "n", "z"])
+
+@dataop_comp_arith
+def test_data_proc_arith(tmpdir, op, armv7op, armv7op_):
+    asm = """
+            mov x0, #{armv7op}
+            mov x1, #{armv7op_}
+            {op} x2, x0, x1
+    """.format(**locals())
+    compare(tmpdir, asm, ["x0","x1", "x2", "x3", "n", "z", "c", "v"])
 
