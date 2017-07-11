18c4336d0c10966f19b3c18abb3d3bbc9562baa8
Sarah Zennou <sarah.zennou@airbus.com>
2017-07-11 11:23:12 +0200

    Multiple taint sources: making Cfa implementation correspond to Cfa signature (type of field taint_sources) again

--- ocaml/src/disassembly/cfa.ml
+++ ocaml/src/disassembly/cfa.ml
@@ -149,7 +149,7 @@ struct
 	  mutable forward_loop: bool; (** true whenever the state belongs to a loop that is forward analysed in CFA mode *)
 	  mutable branch: bool option; (** None is for unconditional predecessor. Some true if the predecessor is a If-statement for which the true branch has been taken. Some false if the false branch has been taken *)
 	  mutable bytes: char list;      (** corresponding list of bytes *)
-	 mutable taint_sources: string (** set of taint sources. Empty if not tainted  *)
+	 mutable taint_sources: Taint.t (** set of taint sources. Empty if not tainted  *)
 	}
       
 	(** the state identificator counter *)
