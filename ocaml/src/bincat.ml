14d01dfbe6ad9a5af7c90645e0338685cf564f31
phil <phil@secdev.org>
2017-07-21 18:16:16 +0200

    Make it possible to compile bincat_native in bytecode to use ocamldebug

--- ocaml/src/bincat.ml
+++ ocaml/src/bincat.ml
@@ -18,6 +18,6 @@
 
 Printf.printf "BinCAT %s\n" Bincat_ver.version_string;
 if (Array.length Sys.argv < 4) then
-        print_endline "Usage: bincat_native init.ini output.ini outlog"
+        print_endline "Usage: bincat init.ini output.ini outlog"
 else
     Main.process Sys.argv.(1) Sys.argv.(2) Sys.argv.(3);;
