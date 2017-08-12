c4a42dc62b558cfe08a6e63eb3ee3c07ea3bc847
phil <phil@secdev.org>
2017-08-12 23:57:56 +0200

    Fix test/libc.c on __sprintf_chk, add __printf_chk and puts

--- test/libc.c
+++ test/libc.c
@@ -1,7 +1,8 @@
 int printf(const char *format, ...) {}
 int sprintf(char *str, const char *format, ...) {}
-int __sprintf_check(char *str, int flag, unsigned int strlen, const char *format, ...) {}
+int __printf_chk(int flag, const char * format) {}
+int __sprintf_chk(char * str, int flag, int strlen, const char * format) {}
 void *memcpy(void *dest, const void *src, unsigned int n) {}
 unsigned int strlen(const char *s) {}
-
+int puts(const char *s) {}
 
