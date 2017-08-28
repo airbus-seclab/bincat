54ba6b56969a7f7eb271708f7c73d79c5707c489
Raphaël Rigo <raphael.rigo@airbus.com>
2017-08-28 12:57:05 +0200

    memcpy in stdio.c (yeah, I know)

--- lib/stdio.c
+++ lib/stdio.c
@@ -2,3 +2,4 @@ int printf(const char *format, ...) {}
 int puts(const char *s) {}
 int sprintf(char *str, const char *format, ...) {}
 int strlen(char *) {}
+void *memcpy(void *, void *, unsigned int) {}
