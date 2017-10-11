9e1b3bccd1ff2ebf2d7e669d96142edb488d9805
phil <phil@secdev.org>
2017-10-11 15:51:26 +0200

    Modified eggloader_armv8 to output SIMD/FP registers

--- test/eggloader_armv8.c
+++ test/eggloader_armv8.c
@@ -20,11 +20,22 @@ int main(int argc, char *argv[])
 {
         int f;
         int len;
-        unsigned long x0, x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13, x14, x15,
-                x16, x17, x18, x19, x20, x21, x22, x23, x24, x25, x26, x27, x28, x29, x30;
-        unsigned int nzcv;
+        void *raddr;
+        typedef struct { unsigned long low,high; } vreg; // 128bit SIMD registers
+        struct {
+                unsigned long x0, x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13, x14, x15,
+                        x16, x17, x18, x19, x20, x21, x22, x23, x24, x25, x26, x27, x28, x29, x30;
+                unsigned long nzcv;
+                vreg q0, q1, q2, q3, q4, q5, q6, q7, q8, q9, q10, q11, q12, q13, q14, q15,
+                     q16, q17, q18, q19, q20, q21, q22, q23, q24, q25, q26, q27, q28, q29, q30, q31;
+        } r;
         void *spsav;
         void *retsav;
+
+        raddr = &r;
+
+        printf("xxx=%li\n", sizeof(unsigned long long));
+        printf("xxx=%li\n", sizeof(unsigned long));
 
         char ret_to_main[] =
                 "\xfd\x03\x1e\xaa"    // mov x29, x30   ; save x30 (lr) from test
@@ -54,13 +65,16 @@ int main(int argc, char *argv[])
         "ldr x0, %[retsav]\n"
         "str x30, [x0]\n"
         "str x29, [sp,#-16]!\n"  // push fp
+        "ldr x0, %[r]\n"
+        "str x0, [sp,#-16]!\n"   // push struct r
         "ldr x0, %[spsav]\n"
         "mov x1, sp\n"
         "str x1, [x0]\n"
         "sub sp, sp, #0x1000\n"  // create some space to mess up with the stack
         : 
         [retsav] "=m" (retsav),
-        [spsav] "=m" (spsav));
+        [spsav] "=m" (spsav),
+        [r] "=m" (raddr));
 
         (*egg)();
 
@@ -68,110 +82,141 @@ int main(int argc, char *argv[])
         ".after:\n"
         "bl .before\n"
         "mov x30, x29\n"       // restore x30 from test
-        "ldr x29, [sp], #16\n" // restore x29 (fp)
+        "ldr x29, [sp], #16\n" // x29 points to struct r
 
-        "str x0, %[reg0]\n"
-        "str x1, %[reg1]\n"
-        "str x2, %[reg2]\n"
-        "str x3, %[reg3]\n"
-        "str x4, %[reg4]\n"
-        "str x5, %[reg5]\n"
-        "str x6, %[reg6]\n"
-        "str x7, %[reg7]\n"
-        "str x8, %[reg8]\n"
-        "str x9, %[reg9]\n"
-        "str x10, %[reg10]\n"
-        "str x11, %[reg11]\n"
-        "str x12, %[reg12]\n"
-        "str x13, %[reg13]\n"
-        "str x14, %[reg14]\n"
-        "str x15, %[reg15]\n"
-        :
-        [reg0] "=m" (x0),
-        [reg1] "=m" (x1),
-        [reg2] "=m" (x2),
-        [reg3] "=m" (x3),
-        [reg4] "=m" (x4),
-        [reg5] "=m" (x5),
-        [reg6] "=m" (x6),
-        [reg7] "=m" (x7),
-        [reg8] "=m" (x8),
-        [reg9] "=m" (x9),
-        [reg10] "=m" (x10),
-        [reg11] "=m" (x11),
-        [reg12] "=m" (x12),
-        [reg13] "=m" (x13),
-        [reg14] "=m" (x14),
-        [reg15] "=m" (x15));
-
-         asm volatile(
-        "str x16, %[reg16]\n"
-        "str x17, %[reg17]\n"
-        "str x18, %[reg18]\n"
-        "str x19, %[reg19]\n"
-        "str x20, %[reg20]\n"
-        "str x21, %[reg21]\n"
-        "str x22, %[reg22]\n"
-        "str x23, %[reg23]\n"
-        "str x24, %[reg24]\n"
-        "str x25, %[reg25]\n"
-        "str x26, %[reg26]\n"
-        "str x27, %[reg27]\n"
-        "str x28, %[reg28]\n"
-        "str x29, %[reg29]\n"
-        "str x30, %[reg30]\n"
+        "str x0, [x29],#8\n"
+        "str x1, [x29],#8\n"
+        "str x2, [x29],#8\n"
+        "str x3, [x29],#8\n"
+        "str x4, [x29],#8\n"
+        "str x5, [x29],#8\n"
+        "str x6, [x29],#8\n"
+        "str x7, [x29],#8\n"
+        "str x8, [x29],#8\n"
+        "str x9, [x29],#8\n"
+        "str x10, [x29],#8\n"
+        "str x11, [x29],#8\n"
+        "str x12, [x29],#8\n"
+        "str x13, [x29],#8\n"
+        "str x14, [x29],#8\n"
+        "str x15, [x29],#8\n"
+        "str x16, [x29],#8\n"
+        "str x17, [x29],#8\n"
+        "str x18, [x29],#8\n"
+        "str x19, [x29],#8\n"
+        "str x20, [x29],#8\n"
+        "str x21, [x29],#8\n"
+        "str x22, [x29],#8\n"
+        "str x23, [x29],#8\n"
+        "str x24, [x29],#8\n"
+        "str x25, [x29],#8\n"
+        "str x26, [x29],#8\n"
+        "str x27, [x29],#8\n"
+        "str x28, [x29],#8\n"
+        "str x30, [x29],#8\n" // value of x29 is lost anyway
+        "str x30, [x29],#8\n"
         "mrs x0, NZCV\n"
-        "str x0, %[nzcv]\n"
-        :
-        [reg16] "=m" (x16),
-        [reg17] "=m" (x17),
-        [reg18] "=m" (x18),
-        [reg19] "=m" (x19),
-        [reg20] "=m" (x20),
-        [reg21] "=m" (x21),
-        [reg22] "=m" (x22),
-        [reg23] "=m" (x23),
-        [reg24] "=m" (x24),
-        [reg25] "=m" (x25),
-        [reg26] "=m" (x26),
-        [reg27] "=m" (x27),
-        [reg28] "=m" (x28),
-        [reg29] "=m" (x29),
-        [reg30] "=m" (x30),
-        [nzcv] "=m" (nzcv));
+        "str x0, [x29], #8\n"
 
-        printf("x0=%016lx\n", x0);
-        printf("x1=%016lx\n", x1);
-        printf("x2=%016lx\n", x2);
-        printf("x3=%016lx\n", x3);
-        printf("x4=%016lx\n", x4);
-        printf("x5=%016lx\n", x5);
-        printf("x6=%016lx\n", x6);
-        printf("x7=%016lx\n", x7);
-        printf("x8=%016lx\n", x8);
-        printf("x9=%016lx\n", x9);
-        printf("x10=%016lx\n", x10);
-        printf("x11=%016lx\n", x11);
-        printf("x12=%016lx\n", x12);
-        printf("x13=%016lx\n", x13);
-        printf("x14=%016lx\n", x14);
-        printf("x15=%016lx\n", x15);
-        printf("x16=%016lx\n", x16);
-        printf("x17=%016lx\n", x17);
-        printf("x18=%016lx\n", x18);
-        printf("x19=%016lx\n", x19);
-        printf("x20=%016lx\n", x20);
-        printf("x21=%016lx\n", x21);
-        printf("x22=%016lx\n", x22);
-        printf("x23=%016lx\n", x23);
-        printf("x24=%016lx\n", x24);
-        printf("x25=%016lx\n", x25);
-        printf("x26=%016lx\n", x26);
-        printf("x27=%016lx\n", x27);
-        printf("x28=%016lx\n", x28);
-        printf("x29=%016lx\n", x29);
-        printf("x29=%016lx\n", x29);
-        printf("nzcv=%08x\n", nzcv);
+        "str q0, [x29],#16\n"
+        "str q1, [x29],#16\n"
+        "str q2, [x29],#16\n"
+        "str q3, [x29],#16\n"
+        "str q4, [x29],#16\n"
+        "str q5, [x29],#16\n"
+        "str q6, [x29],#16\n"
+        "str q7, [x29],#16\n"
+        "str q8, [x29],#16\n"
+        "str q9, [x29],#16\n"
+        "str q10, [x29],#16\n"
+        "str q11, [x29],#16\n"
+        "str q12, [x29],#16\n"
+        "str q13, [x29],#16\n"
+        "str q14, [x29],#16\n"
+        "str q15, [x29],#16\n"
+        "str q16, [x29],#16\n"
+        "str q17, [x29],#16\n"
+        "str q18, [x29],#16\n"
+        "str q19, [x29],#16\n"
+        "str q20, [x29],#16\n"
+        "str q21, [x29],#16\n"
+        "str q22, [x29],#16\n"
+        "str q23, [x29],#16\n"
+        "str q24, [x29],#16\n"
+        "str q25, [x29],#16\n"
+        "str q26, [x29],#16\n"
+        "str q27, [x29],#16\n"
+        "str q28, [x29],#16\n"
+        "str q29, [x29],#16\n"
+        "str q30, [x29],#16\n"
+        "str q31, [x29],#16\n"
+
+        "ldr x29, [sp], #16\n"); // restore x29 (fp)
+
+        printf("x0=%016lx\n", r.x0);
+        printf("x1=%016lx\n", r.x1);
+        printf("x2=%016lx\n", r.x2);
+        printf("x3=%016lx\n", r.x3);
+        printf("x4=%016lx\n", r.x4);
+        printf("x5=%016lx\n", r.x5);
+        printf("x6=%016lx\n", r.x6);
+        printf("x7=%016lx\n", r.x7);
+        printf("x8=%016lx\n", r.x8);
+        printf("x9=%016lx\n", r.x9);
+        printf("x10=%016lx\n", r.x10);
+        printf("x11=%016lx\n", r.x11);
+        printf("x12=%016lx\n", r.x12);
+        printf("x13=%016lx\n", r.x13);
+        printf("x14=%016lx\n", r.x14);
+        printf("x15=%016lx\n", r.x15);
+        printf("x16=%016lx\n", r.x16);
+        printf("x17=%016lx\n", r.x17);
+        printf("x18=%016lx\n", r.x18);
+        printf("x19=%016lx\n", r.x19);
+        printf("x20=%016lx\n", r.x20);
+        printf("x21=%016lx\n", r.x21);
+        printf("x22=%016lx\n", r.x22);
+        printf("x23=%016lx\n", r.x23);
+        printf("x24=%016lx\n", r.x24);
+        printf("x25=%016lx\n", r.x25);
+        printf("x26=%016lx\n", r.x26);
+        printf("x27=%016lx\n", r.x27);
+        printf("x28=%016lx\n", r.x28);
+        printf("x29=%016lx\n", r.x29);
+        printf("x29=%016lx\n", r.x29);
+        printf("x30=%016lx\n", r.x30);
+        printf("nzcv=%016lx\n", r.nzcv);
+        printf("q0=%016lx%016lx\n", r.q0.high, r.q0.low);
+        printf("q1=%016lx%016lx\n", r.q1.high, r.q1.low);
+        printf("q2=%016lx%016lx\n", r.q2.high, r.q2.low);
+        printf("q3=%016lx%016lx\n", r.q3.high, r.q3.low);
+        printf("q4=%016lx%016lx\n", r.q4.high, r.q4.low);
+        printf("q5=%016lx%016lx\n", r.q5.high, r.q5.low);
+        printf("q6=%016lx%016lx\n", r.q6.high, r.q6.low);
+        printf("q7=%016lx%016lx\n", r.q7.high, r.q7.low);
+        printf("q8=%016lx%016lx\n", r.q8.high, r.q8.low);
+        printf("q9=%016lx%016lx\n", r.q9.high, r.q9.low);
+        printf("q10=%016lx%016lx\n", r.q10.high, r.q10.low);
+        printf("q11=%016lx%016lx\n", r.q11.high, r.q11.low);
+        printf("q12=%016lx%016lx\n", r.q12.high, r.q12.low);
+        printf("q13=%016lx%016lx\n", r.q13.high, r.q13.low);
+        printf("q14=%016lx%016lx\n", r.q14.high, r.q14.low);
+        printf("q15=%016lx%016lx\n", r.q15.high, r.q15.low);
+        printf("q16=%016lx%016lx\n", r.q16.high, r.q16.low);
+        printf("q17=%016lx%016lx\n", r.q17.high, r.q17.low);
+        printf("q18=%016lx%016lx\n", r.q18.high, r.q18.low);
+        printf("q19=%016lx%016lx\n", r.q19.high, r.q19.low);
+        printf("q20=%016lx%016lx\n", r.q20.high, r.q20.low);
+        printf("q21=%016lx%016lx\n", r.q21.high, r.q21.low);
+        printf("q22=%016lx%016lx\n", r.q22.high, r.q22.low);
+        printf("q23=%016lx%016lx\n", r.q23.high, r.q23.low);
+        printf("q24=%016lx%016lx\n", r.q24.high, r.q24.low);
+        printf("q25=%016lx%016lx\n", r.q25.high, r.q25.low);
+        printf("q26=%016lx%016lx\n", r.q26.high, r.q26.low);
+        printf("q27=%016lx%016lx\n", r.q27.high, r.q27.low);
+        printf("q28=%016lx%016lx\n", r.q28.high, r.q28.low);
+        printf("q29=%016lx%016lx\n", r.q29.high, r.q29.low);
+        printf("q30=%016lx%016lx\n", r.q30.high, r.q30.low);
+        printf("q31=%016lx%016lx\n", r.q31.high, r.q31.low);
         return 0;
 }
-
