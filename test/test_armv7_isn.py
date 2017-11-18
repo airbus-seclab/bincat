62b4e110e15e97af1fbeda17473a34ed085173ed
phil <phil@secdev.org>
2017-11-18 01:30:17 +0100

    Thumb: add thumb test and easy dual ARM/Thumb tests

--- test/test_armv7_isn.py
+++ test/test_armv7_isn.py
@@ -1,18 +1,29 @@
 import pytest
 import os
-from util import ARM
+from util import ARM,Thumb
 
 arm = ARM(
     os.path.join(os.path.dirname(os.path.realpath(__file__)),'armv7.ini.in')
 )
 compare = arm.compare
 
+thumb = Thumb(
+    os.path.join(os.path.dirname(os.path.realpath(__file__)),'armv7thumb.ini.in')
+)
+tcompare = thumb.compare
 
-def test_nop(tmpdir):
+@pytest.fixture()
+def cmpall():
+    def cmpall_(*args, **kargs):
+        compare(*args, **kargs)
+        tcompare(*args, **kargs)
+    return cmpall_
+
+def test_nop(tmpdir, cmpall):
     asm = """
         mov r0,r0
     """
-    compare(tmpdir, asm, [])
+    cmpall(tmpdir, asm, [])
 
 
 def test_assign(tmpdir):
