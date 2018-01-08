573943b6d5123ae1ac82323fd335f1f2c50987d4
Sarah Zennou <sarah.zennou@airbus.com>
2018-01-08 11:43:24 +0100

    update Cfa.init_heap with a call to Data.Address.new_heap_region

--- ocaml/src/disassembly/cfa.ml
+++ ocaml/src/disassembly/cfa.ml
@@ -219,7 +219,7 @@ struct
         let repeat, nb = fst entry in
         let content = snd entry in
         let content_size = Config.size_of_content content in
-        let heap_region = Data.Address.new_heap_region  in
+        let heap_region = Data.Address.new_heap_region (nb*content_size) in
         let addr' = Data.Address.of_int heap_region !Config.address_sz in
         let d', taint' = Domain.set_memory_from_config addr' Data.Address.Global content nb domain in
         d', Taint.logor prev_taint taint'
