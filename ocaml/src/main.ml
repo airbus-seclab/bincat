a75df44b9ec31f2634b9087b00a43a91b55cac8a
phil <phil@secdev.org>
2018-01-26 18:01:39 +0100

    Dump list of mapped sections in log file

--- ocaml/src/main.ml
+++ ocaml/src/main.ml
@@ -77,7 +77,24 @@ let process (configfile:string) (resultfile:string) (logfile:string): unit =
       | Config.RAW -> Raw.make_mapped_mem
       | Config.MANUAL -> Manual.make_mapped_mem
     in
-    Mapped_mem.current_mapping := Some (do_map_file ());
+    let cur_map = do_map_file () in
+    Mapped_mem.current_mapping := Some cur_map;
+    if L.log_info2 () then
+      begin
+        L.info2(fun p -> p "-- Dump of mapped sections");
+        List.iter
+          (fun sec ->
+            L.info2 (
+                fun p -> p "Mapped section vaddr=%08x-%08x (%#08x bytes) paddr=%08x->%08x (%#08x bytes) %s"
+                           (Z.to_int (Data.Address.to_int sec.Mapped_mem.virt_addr))
+                           (Z.to_int (Data.Address.to_int sec.Mapped_mem.virt_addr_end))
+                           (Z.to_int sec.Mapped_mem.virt_size)
+                           (Z.to_int sec.Mapped_mem.raw_addr)
+                           (Z.to_int sec.Mapped_mem.raw_addr_end)
+                           (Z.to_int sec.Mapped_mem.raw_size)
+                           sec.Mapped_mem.name)) cur_map.Mapped_mem.sections;
+        L.info2(fun p -> p "-- End of mapped sections dump");
+      end;
     let module Vector    = Vector.Make(Reduced_bit_tainting) in
     let module Pointer   = Pointer.Make(Vector) in
     let module Domain    = Reduced_unrel_typenv.Make(Pointer) in
