4d2dcfc359ebc65b5addb4a0270872bd6b8c9bc2
Raphaël Rigo <raphael.rigo@airbus.com>
2018-03-01 13:20:51 +0100

    add constant for R_386_TLS_TPOFF in ELF loader

--- ocaml/src/loaders/elf_core.ml
+++ ocaml/src/loaders/elf_core.ml
@@ -664,7 +664,7 @@ type reloc_type_t =
   | RELOC_OTHER of e_machine_t * int
   (* X86 relocation types *)
   | R_386_NONE | R_386_32 | R_386_PC32 | R_386_GOT32 | R_386_PLT32 | R_386_COPY | R_386_GLOB_DAT
-  | R_386_JUMP_SLOT | R_386_RELATIVE | R_386_GOTOFF | R_386_GOTPC
+  | R_386_JUMP_SLOT | R_386_RELATIVE | R_386_GOTOFF | R_386_GOTPC | R_386_TLS_TPOFF
   (* ARM relocation types *)
   | R_ARM_NONE | R_ARM_GLOB_DAT | R_ARM_JUMP_SLOT
   (* AARCH64 relocation types *)
@@ -679,7 +679,7 @@ let to_reloc_type r hdr =
          match r with
          | 0 -> R_386_NONE  | 1 -> R_386_32        | 2 -> R_386_PC32       | 3 -> R_386_GOT32    | 4 -> R_386_PLT32
          | 5 -> R_386_COPY  | 6 -> R_386_GLOB_DAT  | 7 -> R_386_JUMP_SLOT  | 8 -> R_386_RELATIVE | 9 -> R_386_GOTOFF
-         | 10 -> R_386_GOTPC
+         | 10 -> R_386_GOTPC | 14 -> R_386_TLS_TPOFF
          | _ -> RELOC_OTHER (hdr.e_machine, r)
        end
     | ARM ->
@@ -711,7 +711,8 @@ let reloc_type_to_string rel =
   | R_386_NONE -> "R_386_NONE"          | R_386_32 -> "R_386_32"                | R_386_PC32 -> "R_386_PC32"
   | R_386_GOT32 -> "R_386_GOT32"        | R_386_PLT32 -> "R_386_PLT32"          | R_386_COPY -> "R_386_COPY"
   | R_386_GLOB_DAT -> "R_386_GLOB_DAT"  | R_386_JUMP_SLOT -> "R_386_JUMP_SLOT"  | R_386_RELATIVE -> "R_386_RELATIVE"
-  | R_386_GOTOFF -> "R_386_GOTOFF"      | R_386_GOTPC -> "R_386_GOTPC"          | R_ARM_NONE -> "R_ARM_NONE"
+  | R_386_GOTOFF -> "R_386_GOTOFF"      | R_386_GOTPC -> "R_386_GOTPC"          | R_386_TLS_TPOFF -> "R_386_TLS_TPOFF"
+  | R_ARM_NONE -> "R_ARM_NONE"
   | R_ARM_GLOB_DAT -> "R_ARM_GLOB_DAT"  | R_ARM_JUMP_SLOT -> "R_ARM_JUMP_SLOT"
   | R_AARCH64_COPY -> "R_AARCH64_COPY"                 | R_AARCH64_GLOB_DAT -> "R_AARCH64_GLOB_DAT"
   | R_AARCH64_JUMP_SLOT -> "R_AARCH64_JUMP_SLOT"       | R_AARCH64_RELATIVE -> "R_AARCH64_RELATIVE"
