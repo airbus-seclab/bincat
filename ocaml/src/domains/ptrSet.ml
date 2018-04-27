a7bf875cc33bb0fe6afb3008b36a7126b651970b
Sarah Zennou <sarah.zennou@airbus.com>
2018-04-27 13:16:47 +0200

    data type of PtrSet functor

--- ocaml/src/domains/ptrSet.ml
+++ ocaml/src/domains/ptrSet.ml
@@ -1,5 +1,7 @@
 (* set of pointers *)
 module Make (V: Vector.T) =
   (struct
-      type ptr = Pointer.Make(V)
+    module Ptr = Pointer.Make(V)
+    module PtrSet = Set.Make(module type t = Ptr.t let compare = Ptr.total_order end)
+    type t = PtrSet.t
    end: Unrel.T)
