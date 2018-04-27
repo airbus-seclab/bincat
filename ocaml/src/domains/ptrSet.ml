265a9fd83d09a98f15c36579c7193e1963b72679
Sarah Zennou <sarah.zennou@airbus.com>
2018-04-27 14:27:01 +0200

    enriched PtrSet.t with BOT

--- ocaml/src/domains/ptrSet.ml
+++ ocaml/src/domains/ptrSet.ml
@@ -3,11 +3,17 @@ module Make (V: Vector.T) =
   (struct
     module Ptr = Pointer.Make(V)
     module PtrSet = Set.Make(struct type t = Ptr.t let compare = Ptr.total_order end)
-    type t = PtrSet.t
+    type t =
+      | Val of PtrSet.t 
+      | BOT
 
     let normalize e =
-      if PtrSet.cardinal > !Config.kset_bound then
-        PtrSet.fold (fun v e -> V.join v e) e V.bot
-      else
-        e
+      match e with
+      | BOT -> BOT
+      | Val e' ->
+         if PtrSet.cardinal > !Config.kset_bound then
+           Val (PtrSet.fold (fun v e -> V.join v e) e' V.bot)
+         else
+           e
+             
    end: Unrel.T)
