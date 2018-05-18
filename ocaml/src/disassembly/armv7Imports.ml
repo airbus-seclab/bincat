2c03545ad4fdc52b363f031ff29d2c66329f292c
Sarah Zennou <sarah.zennou@airbus.com>
2018-05-18 11:45:46 +0200

    bug fix in Armv7Imports.skip

--- ocaml/src/disassembly/armv7Imports.ml
+++ ocaml/src/disassembly/armv7Imports.ml
@@ -99,27 +99,27 @@ struct
 
 
   let skip fdesc a =
-    let ia = Data.Address.to_int a in
-    let key, fasm, fdesc', call_conv =
       match fdesc with
-      | Some (fdesc', cc) -> Config.Fun_name fdesc'.Asm.name, Asm.Fun_name fdesc'.name, fdesc', cc
+      | Some (fdesc', cc) ->
+         if Hashtbl.mem Config.funSkipTbl (Config.Fun_name fdesc'.Asm.name) then
+           let stmts = [Directive (Skip (Asm.Fun_name fdesc'.Asm.name, cc))]  in
+           { fdesc' with stub = stmts }
+         else
+           fdesc'
+
       | None ->
-         let fdesc' =
+         let ia = Data.Address.to_int a in
+         if Hashtbl.mem Config.funSkipTbl (Config.Fun_addr ia) then
            {
           name = "";
           libname = "";
           prologue = [];
           stub = [];
-          epilogue = [ Set( reg "t", Lval (preg "lr" 0 0)) ] ;
+          epilogue = [Directive (Skip (Asm.Fun_addr a, get_callconv())) ;Set( reg "t", Lval (preg "lr" 0 0)) ] ;
           ret_addr =BinOp(And, Lval(reg "lr"), const 0xfffffffe 32) ;
            }
-         in
-         Config.Fun_addr ia, Asm.Fun_addr a, fdesc', get_callconv ()
-    in
-    if Hashtbl.mem Config.funSkipTbl key then
-        { fdesc' with stub = [Directive (Skip (fasm, call_conv))]}
-    else
-      raise Not_found
+         else
+           raise Not_found
     
   let init () =
     Stubs.init ();
