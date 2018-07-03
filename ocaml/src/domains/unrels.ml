9c4e7759fae69fee2efad9de54bd78fa8e07a0f2
Sarah Zennou <sarah.zennou@airbus.com>
2018-07-03 15:13:45 +0200

    Unrels.value_of_register

--- ocaml/src/domains/unrels.ml
+++ ocaml/src/domains/unrels.ml
@@ -25,13 +25,36 @@ module Make(D: Unrel.T) =
       | BOT
       | Val of USet.t
 
+    let init () = BOT
+                
     let bot = BOT
             
     let is_bot m = m = BOT
 
+    let imprecise_exn r =
+      raise (Exceptions.too_many_concrete_elements (Printf.sprintf "value of register %s is too much imprecise" (Register.name r)))
+      
+    let value_of_register m r =
+      match m with
+      | BOT -> raise (Exceptions.Empty (Printf.sprintf "unrel.value_of_register:  environment is empty; can't look up register %s" (Register.name r)))
+      | Val m' ->
+         let v = USet.fold (fun u prev ->
+                     let v' = Unrel.value_of_register u r in
+                     match prev with
+                     | None -> Some v'
+                     | Some v ->
+                        if Z.compare z z' = 0 then prev
+                        else imprecise_exn r
+                   ) m' None
+         in
+         begin
+           match v with
+           | None -> imprecise_exn r
+         end
+         
     let string_of_register m r =
       match m with
-      | BOT -> L.error (fun p -> p "value_of_register: environment is empty ; can't look up register %s" (Register.name r))
+      | BOT ->  raise (Exceptions.Empty (Printf.sprintf "string_of_register: environment is empty; can't look up register %s" (Register.name r)))
       | Val m' -> USet.fold (fun acc u -> (Unrel.value_of_register u)^acc) "" m'
              
   end: Domain.T)
