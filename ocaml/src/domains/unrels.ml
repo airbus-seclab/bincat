fe46849e3334d5affba31956f8b3dcd8d7ac9e0f
Sarah Zennou <sarah.zennou@airbus.com>
2018-07-03 12:56:34 +0200

    added bottom value to Unrels

--- ocaml/src/domains/unrels.ml
+++ ocaml/src/domains/unrels.ml
@@ -1,7 +1,28 @@
+(*
+    This file is part of BinCAT.
+    Copyright 2014-2018 - Airbus
+
+    BinCAT is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    BinCAT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with BinCAT.  If not, see <http://www.gnu.org/licenses/>.
+ *)
+
 (* k-set of Unrel *)
 module Make(D: T) =
   (struct
     module U = Unrel.Make(D)
     module USet = Set.Make(struct type t = U.t let compare = U.total_order end)
-    type = USet.t
+    type t =
+      | BOT
+      | Val of USet.t
+             
   end: Domain.T)
