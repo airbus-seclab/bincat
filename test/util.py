818a6ce01156acf5f2752aea0964f2cfcc31b82f
phil <phil@secdev.org>
2018-08-07 14:15:45 +0200

    PPC: add ctr and lr to test infrastructure

--- test/util.py
+++ test/util.py
@@ -394,7 +394,7 @@ class PowerPC(Arch):
     ALL_REGS = [ "r0", "r1", "r2", "r3", "r4", "r5", "r6", "r7", "r8", "r9",
                  "r10", "r11", "r12", "r13", "r14", "r15", "r16", "r17", "r18", "r19",
                  "r20", "r21", "r22", "r23", "r24", "r25", "r26", "r27", "r28", "r29",
-                 "r30", "r31", "lt", "gt", "eq", "so" ]
+                 "r30", "r31", "lr", "ctr", "lt0", "gt0", "eq0", "so0" ]
     # "cr", "lr", "ctr" ]
     AS_TMP_DIR = counter("powerpc-as-%i")
     AS = ["powerpc-linux-gnu-as"]
