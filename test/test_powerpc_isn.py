bafa4bc0b8b602c2109f098a0766805e1e469fa8
phil <phil@secdev.org>
2018-08-07 14:16:27 +0200

    PPC: Add some tests

--- test/test_powerpc_isn.py
+++ test/test_powerpc_isn.py
@@ -8,15 +8,45 @@ compare = powerpc.compare
 
 def test_nop(tmpdir):
     asm = """
+        ori %r0,%r31,0
         nop
     """
     compare(tmpdir, asm, [])
 
 
-def test_assign(tmpdir, cmpall):
+def test_assign(tmpdir):
     asm = """
-        mov r0, #0x12
-        mov r1, r0
-        movs r2, r1
+        lis %r3, 0x1234
+        ori %r3, %r3, 0x5678
     """
-    cmpall(tmpdir, asm, ["r0","r1", "r2", "n", "z"])
+    compare(tmpdir, asm, ["r3"])
+
+def test_assign2(tmpdir):
+    asm = """
+        lis %r3, 0x1234
+        ori %r3, %r3, 0x5678
+        lis %r4, 0xffff
+        ori %r4, %r4, 0xA5A5
+        lis %r5, 0xA1B2
+        ori %r5, %r5, 0xD4C3
+    """
+    compare(tmpdir, asm, ["r3", "r4", "r5"])
+
+def test_add(tmpdir):
+    asm = """
+        lis %r3, 0x1234
+        ori %r3, %r3, 0x5678
+        lis %r4, 0xabcd
+        ori %r4, %r4, 0xffff
+        add %r5, %r3, %r4
+    """
+    compare(tmpdir, asm, ["r3", "r4", "r5" ])
+
+def test_xxx(tmpdir):
+    asm = """
+    sradi %r3, %r4, 5
+    std %r3, 20(%r5)
+    stswi %r3, %r4, 3
+    tlbia
+    """
+    compare(tmpdir, asm, ["r3", "r4", "r5" ])
