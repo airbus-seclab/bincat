e35ff55ed87fc05217a3aeeded11966b7d5943f5
phil <phil@secdev.org>
2018-08-14 14:58:53 +0200

    PPC: factorize arithmetic operation tests

--- test/test_powerpc_isn.py
+++ test/test_powerpc_isn.py
@@ -46,110 +46,62 @@ def test_basics_assign2(tmpdir):
 ## /_/ \_\_| |_|\__|_||_|_|_|_\___|\__|_\__/__/
 ## Arithmetics
 
+ARITH_OPS = ["add", "sub"]
 
-def test_arith_add(tmpdir):
+@pytest.mark.parametrize("op", ARITH_OPS)
+def test_arith_add(tmpdir, op):
     asm = """
         lis %r3, 0x1234
         ori %r3, %r3, 0x5678
         lis %r4, 0xabcd
         ori %r4, %r4, 0xffff
-        add %r5, %r3, %r4
-    """
+        {op} %r5, %r3, %r4
+    """.format(**locals())
     compare(tmpdir, asm, ["r3", "r4", "r5" ])
 
-def test_arith_add_dot(tmpdir):
+@pytest.mark.parametrize("op", ARITH_OPS)
+def test_arith_add_dot(tmpdir, op):
     asm = """
         lis %r3, 0x1234
         ori %r3, %r3, 0x5678
         lis %r4, 0xabcd
         ori %r4, %r4, 0xffff
-        add. %r5, %r3, %r4
-    """
+        {op}. %r5, %r3, %r4
+    """.format(**locals())
     compare(tmpdir, asm, ["r3", "r4", "r5", "cr:29-31" ])
 
-def test_arith_addo(tmpdir):
+@pytest.mark.parametrize("op", ARITH_OPS)
+def test_arith_addo(tmpdir, op):
     asm = """
         lis %r3, 0x1234
         ori %r3, %r3, 0x5678
         lis %r4, 0xabcd
         ori %r4, %r4, 0xffff
-        addo %r5, %r3, %r4
-    """
+        {op}o %r5, %r3, %r4
+    """.format(**locals())
     compare(tmpdir, asm, ["r3", "r4", "r5", "ov" ])
 
-def test_arith_addo_dot(tmpdir):
+@pytest.mark.parametrize("op", ARITH_OPS)
+def test_arith_addo_dot(tmpdir, op):
     asm = """
         lis %r3, 0x1234
         ori %r3, %r3, 0x5678
         lis %r4, 0xabcd
         ori %r4, %r4, 0xffff
-        addo. %r5, %r3, %r4
-    """
+        {op}o. %r5, %r3, %r4
+    """.format(**locals())
     compare(tmpdir, asm, ["r3", "r4", "r5", "cr:29-31", "ov" ])
 
-
-def test_arith_add_flags(tmpdir, op32h, op32l, op32h_, op32l_):
+@pytest.mark.parametrize("op", ARITH_OPS)
+def test_arith_add_flags(tmpdir, op, op32h, op32l, op32h_, op32l_):
     asm = """
         lis %r3, {op32h}
         ori %r3, %r3, {op32l}
         lis %r4, {op32h_}
         ori %r4, %r4, {op32l_}
-        addo. %r5, %r3, %r4
+        {op}o. %r5, %r3, %r4
     """.format(**locals())
     compare(tmpdir, asm, ["r3", "r4", "r5", "cr:29-31", "ov" ])
-
-
-def test_arith_sub(tmpdir):
-    asm = """
-        lis %r3, 0x1234
-        ori %r3, %r3, 0x5678
-        lis %r4, 0xabcd
-        ori %r4, %r4, 0xffff
-        sub %r5, %r3, %r4
-    """
-    compare(tmpdir, asm, ["r3", "r4", "r5" ])
-
-def test_arith_sub_dot(tmpdir):
-    asm = """
-        lis %r3, 0x1234
-        ori %r3, %r3, 0x5678
-        lis %r4, 0xabcd
-        ori %r4, %r4, 0xffff
-        sub. %r5, %r3, %r4
-    """
-    compare(tmpdir, asm, ["r3", "r4", "r5", "cr:29-31" ])
-
-def test_arith_subo(tmpdir):
-    asm = """
-        lis %r3, 0x1234
-        ori %r3, %r3, 0x5678
-        lis %r4, 0xabcd
-        ori %r4, %r4, 0xffff
-        subo %r5, %r3, %r4
-    """
-    compare(tmpdir, asm, ["r3", "r4", "r5", "ov" ])
-
-def test_arith_subo_dot(tmpdir):
-    asm = """
-        lis %r3, 0x1234
-        ori %r3, %r3, 0x5678
-        lis %r4, 0xabcd
-        ori %r4, %r4, 0xffff
-        subo. %r5, %r3, %r4
-    """
-    compare(tmpdir, asm, ["r3", "r4", "r5", "cr:29-31", "ov" ])
-
-
-def test_arith_sub_flags(tmpdir, op32h, op32l, op32h_, op32l_):
-    asm = """
-        lis %r3, {op32h}
-        ori %r3, %r3, {op32l}
-        lis %r4, {op32h_}
-        ori %r4, %r4, {op32l_}
-        subo. %r5, %r3, %r4
-    """.format(**locals())
-    compare(tmpdir, asm, ["r3", "r4", "r5", "cr:29-31", "ov" ])
-
 
 
 
