3155d0906c45ff91253e1127f56ab6f60077f605
phil <phil@secdev.org>
2018-08-20 00:16:12 +0200

    PPC: add test for slw

--- test/test_powerpc_isn.py
+++ test/test_powerpc_isn.py
@@ -272,6 +272,16 @@ def test_logic_rlwnm_dot(tmpdir, op32, op32_, op5, op5_, op5__):
     """.format(**locals())
     compare(tmpdir, asm, ["r3", "r4", "cr:29-31"])
 
+@pytest.mark.parametrize("opshift", [0, 1, 6, 30, 31, 32, 35, 63, 66, 125])
+def test_logic_slw_dot(tmpdir, op32, opshift):
+    asm = """
+        lis %r3, {op32}@h
+        ori %r3, %r3, {op32}@l
+        li %r4, {opshift}
+        slw. %r5, %r3, %r4
+    """.format(**locals())
+    compare(tmpdir, asm, ["r3", "r4", "r5", "cr:29-31"])
+
 
 ##   ___
 ##  / __|___ _ __  _ __  __ _ _ _ ___
