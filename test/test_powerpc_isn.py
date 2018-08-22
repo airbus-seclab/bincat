8daeeae97e4183fd6e4d54d2daed9dfb0c4ccb8c
phil <phil@secdev.org>
2018-08-22 11:19:16 +0200

    PPC: add test for mulli

--- test/test_powerpc_isn.py
+++ test/test_powerpc_isn.py
@@ -209,6 +209,14 @@ def test_arith_mul(tmpdir, op, op32, op32_):
     compare(tmpdir, asm, ["r3", "r4", "r5", "cr:29-31"])
 
 
+def test_arith_mulli(tmpdir, op32, op16_s):
+    asm = """
+        lis %r3, {op32}@h
+        ori %r3, %r3, {op32}@l
+        mulli %r4, %r3, {op16_s}
+    """.format(**locals())
+    compare(tmpdir, asm, ["r3", "r4"])
+
 
 ##  _              _
 ## | |   ___  __ _(_)__
