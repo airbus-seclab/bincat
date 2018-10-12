5b39ee6524cdce127023416ad2f0a8a6bf10abaf
Xavier Mehrenberger <xavier.mehrenberger@airbus.com>
2018-10-12 18:20:11 +0200

    test: fix deprecation warning, style

--- test/test_x86_stubs.py
+++ test/test_x86_stubs.py
@@ -4,25 +4,25 @@ from util import X86
 
 
 x86 = X86(
-    os.path.join(os.path.dirname(os.path.realpath(__file__)),'x86.ini.in')
+    os.path.join(os.path.dirname(os.path.realpath(__file__)), 'x86.ini.in')
 )
 
 xfail = pytest.mark.skip
 
-@pytest.mark.parametrize("val",[0, 1, 0x1f2, 0x1fa4, 0x45f672, 0x8245fa3d, 0xffffffff])
-@pytest.mark.parametrize("fmt",[xfail("i"),
-                                "x",
-                                xfail("d"),
-                                xfail("lx")])
-@pytest.mark.parametrize("mod",["",
-                                xfail("+"),
-                                xfail("-")])
-@pytest.mark.parametrize("zeropad",["","0"])
-@pytest.mark.parametrize("sz",["","1","5","8","13"])
+@pytest.mark.parametrize("val", [0, 1, 0x1f2, 0x1fa4, 0x45f672, 0x8245fa3d, 0xffffffff])
+@pytest.mark.parametrize("fmt", [pytest.param("i", marks=pytest.mark.xfail),
+                                 "x",
+                                 pytest.param("d", marks=pytest.mark.xfail),
+                                 pytest.param("lx", marks=pytest.mark.xfail)])
+@pytest.mark.parametrize("mod", ["",
+                                 pytest.param("+", marks=pytest.mark.xfail),
+                                 pytest.param("-", marks=pytest.mark.xfail)])
+@pytest.mark.parametrize("zeropad", ["", "0"])
+@pytest.mark.parametrize("sz", ["", "1", "5", "8", "13"])
 def test_printf_num(tmpdir, val, fmt, mod, zeropad, sz):
     fmtstr = "TEST[%{mod}{zeropad}{sz}{fmt}]".format(**locals())
     asm = """
-        push {val:#x} 
+        push {val:#x}
         push 0x200      ; {fmt!r}
         call 0x80000000
     """.format(val=val, fmt=fmtstr)
@@ -32,14 +32,19 @@ def test_printf_num(tmpdir, val, fmt, mod, zeropad, sz):
     bc.run()
 
     expected = fmtstr % val
-    
+
     assert expected == bc.get_stdout().strip(), (repr(fmtstr)+"\n"+bc.listing)
     assert len(expected) == bc.result.last_reg("eax").value, (repr(fmtstr)+"\n"+bc.listing)
 
 
-@pytest.mark.parametrize("numl",["", xfail("0"), "2", "5"])
-@pytest.mark.parametrize("numr",["" ,xfail(".0"), xfail(".2"), xfail(".5")])
-@pytest.mark.parametrize("val",["","abc", "abcdefghi"])
+@pytest.mark.parametrize("numl", ["",
+                                  pytest.param("0", marks=pytest.mark.xfail),
+                                  "2", "5"])
+@pytest.mark.parametrize("numr", ["",
+                                  pytest.param(".0", marks=pytest.mark.xfail),
+                                  pytest.param(".2", marks=pytest.mark.xfail),
+                                  pytest.param(".5", marks=pytest.mark.xfail)])
+@pytest.mark.parametrize("val", ["", "abc", "abcdefghi"])
 def test_printf_string(tmpdir, val, numl, numr):
     fmtstr = "TEST[%{numl}{numr}s]".format(**locals())
     asm = """
@@ -54,25 +59,25 @@ def test_printf_string(tmpdir, val, numl, numr):
     bc.run()
 
     expected = fmtstr % val
-    
+
     assert expected == bc.get_stdout(), (repr(fmtstr)+"\n"+bc.listing)
     assert len(expected) == bc.result.last_reg("eax").value, (repr(fmtstr)+"\n"+bc.listing)
 
 
-@pytest.mark.parametrize("val",[0, 1, 0x1f2, 0x1fa4, 0x45f672, 0x8245fa3d, 0xffffffff])
-@pytest.mark.parametrize("fmt",[xfail("i"),
-                                "x",
-                                xfail("d"),
-                                xfail("lx")])
-@pytest.mark.parametrize("mod",["",
-                                xfail("+"),
-                                xfail("-")])
-@pytest.mark.parametrize("zeropad",["","0"])
-@pytest.mark.parametrize("sz",["","1","5","8","13"])
+@pytest.mark.parametrize("val", [0, 1, 0x1f2, 0x1fa4, 0x45f672, 0x8245fa3d, 0xffffffff])
+@pytest.mark.parametrize("fmt", [pytest.param("i", marks=pytest.mark.xfail),
+                                 "x",
+                                 pytest.param("d", marks=pytest.mark.xfail),
+                                 pytest.param("lx", marks=pytest.mark.xfail)])
+@pytest.mark.parametrize("mod", ["",
+                                 pytest.param("+", marks=pytest.mark.xfail),
+                                 pytest.param("-", marks=pytest.mark.xfail)])
+@pytest.mark.parametrize("zeropad", ["", "0"])
+@pytest.mark.parametrize("sz", ["", "1", "5", "8", "13"])
 def test_sprintf_num(tmpdir, val, fmt, mod, zeropad, sz):
     fmtstr = "TEST[%{mod}{zeropad}{sz}{fmt}]".format(**locals())
     asm = """
-        push {val:#x} 
+        push {val:#x}
         push 0x200      ; {fmt!r}
         push 0x100
         call 0x80000001
@@ -89,9 +94,14 @@ def test_sprintf_num(tmpdir, val, fmt, mod, zeropad, sz):
     assert len(expected) == bc.result.last_reg("eax").value, (repr(fmtstr)+"\n"+bc.listing)
 
 
-@pytest.mark.parametrize("numl",["", xfail("0"), "2", "5"])
-@pytest.mark.parametrize("numr",["" ,xfail(".0"), xfail(".2"), xfail(".5")])
-@pytest.mark.parametrize("val",["","abc", "abcdefghi"])
+@pytest.mark.parametrize("numl", ["",
+                                  pytest.param("0", marks=pytest.mark.xfail),
+                                  "2", "5"])
+@pytest.mark.parametrize("numr", ["",
+                                  pytest.param(".0", marks=pytest.mark.xfail),
+                                  pytest.param(".2", marks=pytest.mark.xfail),
+                                  pytest.param(".5", marks=pytest.mark.xfail)])
+@pytest.mark.parametrize("val", ["", "abc", "abcdefghi"])
 def test_sprintf_string(tmpdir, val, numl, numr):
     fmtstr = "TEST[%{numl}{numr}s]".format(**locals())
     asm = """
@@ -113,7 +123,7 @@ def test_sprintf_string(tmpdir, val, numl, numr):
     assert len(expected) == bc.result.last_reg("eax").value, (repr(fmtstr)+"\n"+bc.listing)
 
 
-@pytest.mark.parametrize("src",["", "TEST", "X"*41])
+@pytest.mark.parametrize("src", ["", "TEST", "X"*41])
 def test_memcpy_call(tmpdir, src):
     asm = """
            push {length}
@@ -121,7 +131,7 @@ def test_memcpy_call(tmpdir, src):
            push 0x10000
            call 0x80000002
     """.format(length=len(src))
-    
+
     bc = x86.make_bc_test(tmpdir, asm)
     bc.initfile.set_mem(0x20000, src+"\0")
     bc.initfile.set_mem(0x10000, "\0"*100)
@@ -129,8 +139,8 @@ def test_memcpy_call(tmpdir, src):
 
     assert src == bc.result.last_node.get_string("", 0x10000), (repr(src)[:20]+"\n"+bc.listing)
     assert 0x10000 == bc.result.last_reg("eax").value, (repr(src)[:20]+"\n"+bc.listing)
-    
-@pytest.mark.parametrize("src",["", "TEST", "X"*41])
+
+@pytest.mark.parametrize("src", ["", "TEST", "X"*41])
 def test_memcpy_push_ret(tmpdir, src):
     asm = """
            push {length}
