ef1f0cdf2addf19382a390ba1ef6190543e655bd
Sarah Zennou <sarah.zennou@airbus.com>
2019-05-17 16:31:51 +0200

    changed the decoding of x64 instructions that are zero extended

--- ocaml/src/disassembly/core_x86.ml
+++ ocaml/src/disassembly/core_x86.ml
@@ -289,7 +289,7 @@ module  X64(Domain: Domain.T)(Stubs: Stubs.T with type domain_t := Domain.t) =
         | M(_, _) -> normal_set
         | V(r) -> match r with
           | T(_) -> (* full assignation *) normal_set
-          | P(reg, l, u) -> if l == 0 && u == 31 then normal_set @ [ Set(V( P(reg, 32, 63)), const0 32) ] else normal_set
+          | P(reg, l, u) -> if l == 0 && u == 31 then [Set(V(T reg), UnOp(ZeroExt 64, value))]  else normal_set
 
     let add_segment segments operand_sz rip offset sreg =
       match (Register.name sreg) with
