3c58118bd3698a72ee48d1755719b64343112ea4
Raphaël Rigo <raphael.rigo@airbus.com>
2019-11-22 18:09:55 +0100

    update netnode.py to latest version for IDA compatibility

--- python/idabincat/netnode.py
+++ python/idabincat/netnode.py
@@ -9,13 +9,21 @@ import logging
 
 import idaapi
 
-
-CHUNK_SIZE = 1024
+BLOB_SIZE = 1024
 OUR_NETNODE = "$ com.bincat"
-CHUNK_INDEX_TAG = "M"
-CHUNK_TAG = "N"
-g_logger = logging.getLogger("netnode")
-g_logger.setLevel(logging.INFO)
+INT_KEYS_TAG = 'M'
+STR_KEYS_TAG = 'N'
+STR_TO_INT_MAP_TAG = 'O'
+INT_TO_INT_MAP_TAG = 'P'
+logger = logging.getLogger(__name__)
+
+# get the IDA version number
+ida_major, ida_minor = list(map(int, idaapi.get_kernel_version().split(".")))
+using_ida7api = (ida_major > 6)
+
+
+class NetnodeCorruptError(RuntimeError):
+    pass
 
 
 class Netnode(object):
@@ -35,51 +43,34 @@ class Netnode(object):
       numbers, and allows values to be larger than 1024 bytes in length.
     
     This class supports keys that are numbers or strings. 
-    Values must be JSON-encodable.
+    Values must be JSON-encodable. They can not be None.
    
     Implementation:
+     (You don't have to worry about this section if you just want to
+        use the library. Its here for potential contributors.)
+
       The major limitation of the underlying netnode API is the fixed
         maximum length of a value. Values must not be larger than 1024
-        bytes.
+        bytes. Otherwise, you must use the `blob` API. We do that for you.
     
       The first enhancement is transparently zlib-encoding all values.
 
-      To support arbitrarily sized values, we split the value data into
-        chunks (each of length 1024), and store them in a separate
-        "chunk store". The locations of these chunks are stored in a
-        "index store". If a fetch operation encounters a value with length
-        1024 bytes, it also checks the index store to recover the remaining
-        data.
+      To support arbitrarily sized values with keys of either int or str types,
+        we store the values in different places:
     
-      The chunk and index stores are implemented using non-standard netnode
-        hashes/arrays (read the netnode.hpp documentation). The primary
-        key-value data is stored in the default supval or hashval.
-      The chunk store is the supval array identified by the character "N".
-        Chunks are always stored as contiguous runs of supval indices.
-        Data is always stored at LAST_INDEX + 1.
-      The chunk index store is the hashval or supval index indentified by the
-        character "M". The key is the primary key used in the set operation.
-        the value is the json-encoded tuple (start_index, end_index) for the
-        chunk data in the chunk store.
-
-
-        netnode: NAMESPACE
-        +------------------------+
-        | default hashval/supval | ---> { KEY: VALUE[:1024] }
-        +------------------------+
-        | hashval/supval "M"     | ---> { KEY: (4444, 5555) }
-        +------------------------+
-        | supval "N"             | ---> { 
-        |                        |         4444: VALUE[1024:2048]
-        |                        |         4445: VALUE[2048:3092]
-        |                        |          ... 
-        |                        |         5555: VALUE[...:...]
-        |                        |      }
-        +------------------------+
+        - integer keys with small values: stored in default supval table
+        - integer keys with large values: the data is stored in the blob
+           table named 'M' using an internal key. The link from the given key
+           to the internal key is stored in the supval table named 'P'.
+        - string keys with small values: stored in default hashval table
+        - string keys with large values: the data is stored in the blob
+           table named 'N' using an integer key. The link from string key
+           to int key is stored in the supval table named 'O'.
     """
+
     def __init__(self, netnode_name=OUR_NETNODE):
         self._netnode_name = netnode_name
-        #self._n = idaapi.netnode(netnode_name, namelen=0, do_create=True)
+        # self._n = idaapi.netnode(netnode_name, namelen=0, do_create=True)
         self._n = idaapi.netnode(netnode_name, 0, True)
 
     @staticmethod
@@ -98,120 +89,151 @@ class Netnode(object):
     def _decode(data):
         return json.loads(data)
 
-    def __getitem__(self, key):
-        fget = None
-        if isinstance(key, basestring):
-            fget = self._n.hashval
-        elif isinstance(key, (int, long)):
-            fget = self._n.supval
+    def _intdel(self, key):
+        assert isinstance(key, int)
+
+        did_del = False
+        storekey = self._n.supval(key, INT_TO_INT_MAP_TAG)
+        if storekey is not None:
+            storekey = int(storekey)
+            self._n.delblob(storekey, INT_KEYS_TAG)
+            self._n.supdel(key)
+            did_del = True
+        if self._n.supval(key) is not None:
+            self._n.supdel(key)
+            did_del = True
+
+        if not did_del:
+            raise KeyError("'{}' not found".format(key))
+
+    def _get_next_slot(self, tag):
+        '''
+        get the first unused supval table key, or 0 if the
+         table is empty.
+        useful for filling the supval table sequentially.
+        '''
+        slot = self._n.suplast(tag)
+        if slot is None or slot == idaapi.BADNODE:
+            return 0
         else:
-            raise TypeError("cannot use {} as key".format(type(key)))
+            return slot + 1
+
+    def _intset(self, key, value):
+        assert isinstance(key, int)
+        assert value is not None
 
         try:
-            v = fget(key)
-        except TypeError:
-            raise KeyError("'{}' not found".format(key))
-        if v is None:
+            self._intdel(key)
+        except KeyError:
+            pass
+
+        if len(value) > BLOB_SIZE:
+            storekey = self._get_next_slot(INT_KEYS_TAG)
+            self._n.setblob(value, storekey, INT_KEYS_TAG)
+            self._n.supset(key, str(storekey), INT_TO_INT_MAP_TAG)
+        else:
+            self._n.supset(key, value)
+
+    def _intget(self, key):
+        assert isinstance(key, int)
+
+        storekey = self._n.supval(key, INT_TO_INT_MAP_TAG)
+        if storekey is not None:
+            storekey = int(storekey)
+            v = self._n.getblob(storekey, INT_KEYS_TAG)
+            if v is None:
+                raise NetnodeCorruptError()
+            return v
+
+        v = self._n.supval(key)
+        if v is not None:
+            return v
+
+        raise KeyError("'{}' not found".format(key))
+
+    def _strdel(self, key):
+        assert isinstance(key, (str))
+
+        did_del = False
+        storekey = self._n.hashval(key, STR_TO_INT_MAP_TAG)
+        if storekey is not None:
+            storekey = int(storekey)
+            self._n.delblob(storekey, STR_KEYS_TAG)
+            self._n.hashdel(key)
+            did_del = True
+        if self._n.hashval(key):
+            self._n.hashdel(key)
+            did_del = True
+
+        if not did_del:
             raise KeyError("'{}' not found".format(key))
 
-        if len(v) == CHUNK_SIZE:
-            chunks = [v]
-            index_refs = fget(key, CHUNK_INDEX_TAG)
-            if index_refs is not None:
-                first, last = self._decode(self._decompress(index_refs))
-                g_logger.debug("get: chunk run: 0x%x to 0x%x (0x%x chunks)",
-                               first, last, last-first+1)
-                for index_ref in range(first, last + 1):
-                    chunk = self._n.supval(index_ref, CHUNK_TAG)
-                    chunks.append(chunk)
-            g_logger.debug("get: fetched 0x%x chunks", len(chunks))
-            g_logger.debug("get: data length: 0x%x", len("".join(chunks)))
-            return self._decode(self._decompress("".join(chunks)))
+    def _strset(self, key, value):
+        assert isinstance(key, (str))
+        assert value is not None
+
+        try:
+            self._strdel(key)
+        except KeyError:
+            pass
+
+        if len(value) > BLOB_SIZE:
+            storekey = self._get_next_slot(STR_KEYS_TAG)
+            self._n.setblob(value, storekey, STR_KEYS_TAG)
+            self._n.hashset(key, str(storekey), STR_TO_INT_MAP_TAG)
         else:
-            return self._decode(self._decompress(v))
+            self._n.hashset(key, value)
+
+    def _strget(self, key):
+        assert isinstance(key, (str))
+
+        storekey = self._n.hashval(key, STR_TO_INT_MAP_TAG)
+        if storekey is not None:
+            storekey = int(storekey)
+            v = self._n.getblob(storekey, STR_KEYS_TAG)
+            if v is None:
+                raise NetnodeCorruptError()
+            return v
+
+        v = self._n.hashval(key)
+        if v is not None:
+            return v
+
+        raise KeyError("'{}' not found".format(key))
+
+    def __getitem__(self, key):
+        if isinstance(key, str):
+            v = self._strget(key)
+        elif isinstance(key, int):
+            v = self._intget(key)
+        else:
+            raise TypeError("cannot use {} as key".format(type(key)))
+
+        return self._decode(self._decompress(v))
 
     def __setitem__(self, key, value):
-        fset = None
-        fget = None
-        flast = None
-        if isinstance(key, basestring):
-            fset = self._n.hashset
-            fget = self._n.hashval
-            flast = self._n.hashlast
-        elif isinstance(key, (int, long)):
-            fset = self._n.supset
-            fget = self._n.supval
-            flast = self._n.suplast
+        '''
+        does not support setting a value to None.
+        value must be json-serializable.
+        key must be a string or integer.
+        '''
+        assert value is not None
+
+        v = self._compress(self._encode(value))
+        if isinstance(key, str):
+            self._strset(key, v)
+        elif isinstance(key, int):
+            self._intset(key, v)
         else:
             raise TypeError("cannot use {} as key".format(type(key)))
-
-        d = self._compress(self._encode(value))
-        g_logger.debug("set: data length: 0x%x", len(d))
- 
-        # always store first chunk in the hashval table
-        fset(key, d[:CHUNK_SIZE])
-
-        # store remaining chunks in the supval table CHUNK_TAG.
-        # indices are stored in the hashval table CHUNK_INDEX_TAG
-        # using `key`.
-        if len(d) > 1024:
-            # delete existing chunks
-            index_refs = fget(key, CHUNK_INDEX_TAG)
-            if index_refs is not None:
-                first, last = self._decode(self._decompress(index_refs))
-                for index_ref in range(first, last + 1):
-                    self._n.supdel(index_ref, CHUNK_TAG)
-
-            # add each chunk of data to the next available supval slot
-            chunk_refs = []
-            for i in range(CHUNK_SIZE, len(d), CHUNK_SIZE):
-                chunk = d[i:i+CHUNK_SIZE]
-
-                # race, but we're already dealing with writing to the IDB...
-                # note: we assume all these allocations are continuous!
-                chunk_ref = self._n.suplast(CHUNK_TAG)
-                if chunk_ref == idaapi.BADNODE or chunk_ref is None:
-                    chunk_ref = 0
-                chunk_ref += 1
-
-                self._n.supset(chunk_ref, chunk, CHUNK_TAG)
-                chunk_refs.append(chunk_ref)
-
-            first = chunk_refs[0]
-            last = chunk_refs[-1]
-            g_logger.debug("set: chunk run: 0x%x to 0x%x (0x%x chunks)",
-                           first, last, last-first+1)
-            refs = self._compress(self._encode((first, last)))
-            
-            if len(refs) > CHUNK_SIZE:
-                raise BufferError()
-
-            fset(key, refs, CHUNK_INDEX_TAG)
 
     def __delitem__(self, key):
-        if key not in self:
-            raise KeyError("'{}' not found".format(key))
-
-        fdel = None
-        fget = None
-        if isinstance(key, basestring):
-            fdel = self._n.hashdel
-            fget = self._n.hashval
-        elif isinstance(key, (int, long)):
-            fdel = self._n.supdel
-            fget = self._n.supval
+        if isinstance(key, str):
+            self._strdel(key)
+        elif isinstance(key, int):
+            self._intdel(key)
         else:
             raise TypeError("cannot use {} as key".format(type(key)))
-
-        fdel(key)
-        
-        # delete existing chunks
-        index_refs = fget(key, CHUNK_INDEX_TAG)
-        if index_refs is not None:
-            first, last = self._decode(self._decompress(index_refs))
-            for index_ref in range(first, last + 1):
-                self._n.supdel(index_ref, CHUNK_TAG)
-            fdel(key, CHUNK_INDEX_TAG)
 
     def get(self, key, default=None):
         try:
@@ -228,35 +250,72 @@ class Netnode(object):
             return False
 
     def iterkeys(self):
-        ret = []
-        i = self._n.sup1st()
+        # integer keys for all small values
+        i = None
+        if using_ida7api:
+            i = self._n.supfirst()
+        else:
+            i = self._n.sup1st()
         while i != idaapi.BADNODE:
             yield i
-            i = self._n.supnxt(i)
+            if using_ida7api:
+                i = self._n.supnext(i)
+            else:
+                i = self._n.supnxt(i)
 
-        i = self._n.hash1st()
+        # integer keys for all big values
+        if using_ida7api:
+            i = self._n.supfirst(INT_TO_INT_MAP_TAG)
+        else:
+            i = self._n.sup1st(INT_TO_INT_MAP_TAG)
+        while i != idaapi.BADNODE:
+            yield i
+            if using_ida7api:
+                i = self._n.supnext(i, INT_TO_INT_MAP_TAG)
+            else:
+                i = self._n.supnxt(i, INT_TO_INT_MAP_TAG)
+
+        # string keys for all small values
+        if using_ida7api:
+            i = self._n.hashfirst()
+        else:
+            i = self._n.hash1st()
         while i != idaapi.BADNODE and i is not None:
             yield i
-            i = self._n.hashnxt(i)
+            if using_ida7api:
+                i = self._n.hashnext(i)
+            else:
+                i = self._n.hashnxt(i)
+
+        # string keys for all big values
+        if using_ida7api:
+            i = self._n.hashfirst(STR_TO_INT_MAP_TAG)
+        else:
+            i = self._n.hash1st(STR_TO_INT_MAP_TAG)
+        while i != idaapi.BADNODE and i is not None:
+            yield i
+            if using_ida7api:
+                i = self._n.hashnext(i, STR_TO_INT_MAP_TAG)
+            else:
+                i = self._n.hashnxt(i, STR_TO_INT_MAP_TAG)
 
     def keys(self):
-        return [k for k in self.iterkeys()]
+        return [k for k in self.keys()]
 
     def itervalues(self):
-        for k in self.iterkeys():
+        for k in self.keys():
             yield self[k]
 
     def values(self):
-        return [v for v in self.itervalues()]
+        return [v for v in self.values()]
 
     def iteritems(self):
-        for k in self.iterkeys():
+        for k in self.keys():
             yield k, self[k]
 
     def items(self):
-        return [(k, v) for k, v in self.iteritems()]
+        return [(k, v) for k, v in self.items()]
 
     def kill(self):
         self._n.kill()
         self._n = idaapi.netnode(self._netnode_name, 0, True)
-
