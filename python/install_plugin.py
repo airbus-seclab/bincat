6768f89731c84a52e947f4996e3933cdf323ccb1
Raphaël Rigo <raphael.rigo@airbus.com>
2021-11-23 15:40:42 +0100

    Update install_plugin.py

--- python/install_plugin.py
+++ python/install_plugin.py
@@ -9,24 +9,7 @@ try:
     import requests
     print("'requests' is installed, good.")
 except ImportError:
-    if idaapi.ask_yn(idaapi.ASKBTN_NO,
-             "'requests' is not installed, do you want to install it ?\n"
-             "Choose 'no' if you do not intend to use "
-             "a distant BinCAT server") == idaapi.ASKBTN_YES:
-        print("requests is not installed, trying to install")
-        import pip
-        # Fugly hack (cause IDA console is not a real one)
-        saved_stdout = sys.stdout
-        saved_stderr = sys.stderr
-        sys.stdout = StringIO()
-        sys.stderr = StringIO()
-        pip.main(['install', "requests"])
-        sys.stdout.seek(0)
-        sys.stderr.seek(0)
-        saved_stdout.write(sys.stdout.read())
-        saved_stderr.write(sys.stderr.read())
-        sys.stdout = saved_stdout
-        sys.stderr = saved_stderr
+    print("requests is not installed, install it manually for remote server access")
 
 plugin_dir = os.path.join(idaapi.get_user_idadir(), "plugins")
 if not os.path.isdir(plugin_dir):
@@ -52,8 +35,9 @@ if os.path.isdir(src_idabincat) and os.path.isdir(src_pybincat):
                     os.path.join(plugin_dir, "bcplugin.py"))
     except OSError as e:
         print("Could not install! Error: "+str(e)+"\n")
+    else:
+        print("Plugin installed, please restart IDA to use BinCAT")
 else:
     print("ERROR: %s or %s are not existing directories" % (src_idabincat, src_pybincat))
 
 
-print("Plugin installed, please restart IDA to use BinCAT")
